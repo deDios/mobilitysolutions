fbb0ebc00d5de9ff31decab523711ed7d12fa9a6
Pablo de Dios <4855797@gmail.com>
2024-09-01 06:02:25 -0600

    Agregue cambio al titulo con mas descripcion 2

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
-
-echo "Mi primer pagina 2 y mi nombre es Pablo de Dios "
-
+include "Conexion.php";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hola nueva actualización</h1>
+</body>
+</html>
