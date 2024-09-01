b5ea695a45e8ff590b7ce3bc94d3198e9c06fe67
Pablo de Dios <4855797@gmail.com>
2024-09-01 16:11:41 -0600

    Enlace a catalogo v4

--- catalogo.php
+++ catalogo.php
@@ -7,8 +7,22 @@ include "Conexion.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Catálogo</title>
+    <link rel="stylesheet" href="CSS/catalogo.css">
  </head>
  <body>
+    <header>
+        <a href="index.php" class="logo">
+            <img src="logo_MSC.png" alt="Logo de la compañia">
+            <h2 class="Nombre de la empresa">
+                Mobility Solutions Corporation
+            </h2>
+        </a>
+        <nav>
+            <a href="catalogo.php" class="nav-link">Catálogo</a>
+            <a href="" class="nav-link">Sobre nosotros</a>
+            <a href="" class="nav-link">Contacto</a>
+        </nav>
+    </header>
     <h1>Catálogo</h1>
  </body>
  </html>
