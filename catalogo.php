2affa3b1c90ca8bf7693daede5daaa20cb47a473
Pablo de Dios <4855797@gmail.com>
2024-09-11 03:58:11 -0600

    Cambios en clase de figure v54

--- catalogo.php
+++ catalogo.php
@@ -51,6 +51,39 @@ include "Conexion.php";
                 <button>Detalle</button>
             </div>
         </div>
+        <div class="item">
+            <figure>
+                <img src="Imagenes/Catalogo/Auto 1/Img01.jpg" alt="Auto 1">
+            </figure>
+            <div class="info-producto">
+                <h2>Versa Nissans 2025</h2>
+                Plaza Andares, Jal. CP 44940 | 40,000KM
+                <p class="Precio">$150,000</p>
+                <button>Detalle</button>
+            </div>
+        </div>
+        <div class="item">
+            <figure>
+                <img src="Imagenes/Catalogo/Auto 1/Img01.jpg" alt="Auto 1">
+            </figure>
+            <div class="info-producto">
+                <h2>Versa Nissans 2025</h2>
+                Plaza Andares, Jal. CP 44940 | 40,000KM
+                <p class="Precio">$150,000</p>
+                <button>Detalle</button>
+            </div>
+        </div>
+        <div class="item">
+            <figure>
+                <img src="Imagenes/Catalogo/Auto 1/Img01.jpg" alt="Auto 1">
+            </figure>
+            <div class="info-producto">
+                <h2>Versa Nissans 2025</h2>
+                Plaza Andares, Jal. CP 44940 | 40,000KM
+                <p class="Precio">$150,000</p>
+                <button>Detalle</button>
+            </div>
+        </div>
         
     </div>
  </body>
