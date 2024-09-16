5e40e065c7168d523f9146206ddec68af44dbcf0
Pablo de Dios <4855797@gmail.com>
2024-09-16 16:26:43 -0600

    Conexion v15

--- catalogo.php
+++ catalogo.php
@@ -1,9 +1,10 @@
 <?php
     
     $inc = include "Conexion.php";    
+
         if ($inc){
             $consulta = "select * from mobility_solutions.tmx_auto";
-            $resultado = mysqli_query($con,$consulta);  
+            $resultado = mysqli_query($con,$consulta) or die ('Error: '. mysqli_error($con));  
             if ($resultado){
                 while($row = $resultado->fetch_array());
                     $id = $row['id'];
