062b3882a9cb1e327b49a29d1d15b30f3f3cb3aa
Pablo de Dios <4855797@gmail.com>
2024-09-16 15:57:22 -0600

    Conexion v7

--- catalogo.php
+++ catalogo.php
@@ -1,6 +1,5 @@
 <?php
-$inc = include "Conexion.php";
-    
+$inc = include "Conexion.php";    
     if ($inc){
         $consulta = "select * from mobility_solutions.tmx_auto";
         $resultado = mysqli_query($con,$consulta);  
@@ -10,11 +9,8 @@ $inc = include "Conexion.php";
                 $nombre = $row['nombre'];
                 $modelo = $row['modelo'];
                 $marca = $row['marca'];
-        }
-          
+        }    
     }
-    
-
 ?>
  <!DOCTYPE html>
  <html lang="en">
@@ -65,7 +61,7 @@ $inc = include "Conexion.php";
                     </figure>
                     <div class="info-producto">
                         <div class="titulo_marca">
-                            <div class="titulo_carro"> <?php echo $nombre; ?>  </div>
+                            <div class="titulo_carro">  Titulo  </div>
                             <img src="Imagenes/Marcas/logo_nissan.jpg" alt="logo 1">
                         </div>
                         <div class="version_unidad">5 PTS ADVANCE 16L TA A/AC VE RA-16-2021</div>
