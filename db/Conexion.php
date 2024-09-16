b444e740636a004d88895d3cba56f52605ed19dc
Pablo de Dios <4855797@gmail.com>
2024-09-16 16:21:41 -0600

    Conexion v14

--- db/Conexion.php
+++ db/Conexion.php
@@ -1,8 +1,6 @@
 <?php
 $con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "/home/Catalogo/db/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_ssl_set($con,NULL,NULL, "/home/site/wwwroot/Catalogo/db/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($con, "mobilitysolutions-server.mysql.database.azure.com", "btdonyajwn", "h5n9NG>3ktmqph", "mobility_solutions", 3306, MYSQLI_CLIENT_SSL)
-or die ('Error: '. mysqli_error($con));
-
-
+or die ('Error: Conexión no lograda'. mysqli_error($con));
 ?>
