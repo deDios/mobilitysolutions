e2e8c10649d8ca03ec6efaf688b361fca62365eb
Pablo de Dios <4855797@gmail.com>
2024-09-16 15:11:05 -0600

    Conexion v2

--- catalogo.php
+++ catalogo.php
@@ -1,5 +1,17 @@
 <?php
-include "Conexion.php";
+$inc = include "Conexion.php";
+    if ($inc){
+        $consulta = "select * from mobility_solutions.tmx_auto";
+        $resultado = mysqli_query($conex,$consulta);  
+        if ($resultado){
+            while($row = $resultado->fetch_array());
+                $id = $row['id'];
+                $nombre = $row['nombre'];
+                $modelo = $row['modelo'];
+                $marca = $row['marca'];
+        }      
+}
+
 ?>
  <!DOCTYPE html>
  <html lang="en">
@@ -12,6 +24,7 @@ include "Conexion.php";
  </head>
  <body>
 
+ 
     <header>
         <a href="index.php" class="logo" title="Home">
             <img src="logo_MSC.png" alt="Logo de la compañia">
@@ -49,7 +62,7 @@ include "Conexion.php";
                     </figure>
                     <div class="info-producto">
                         <div class="titulo_marca">
-                            <div class="titulo_carro">Nissan Kicks 2021</div>
+                            <div class="titulo_carro"> <?php echo $nombre; ?> </div>
                             <img src="Imagenes/Marcas/logo_nissan.jpg" alt="logo 1">
                         </div>
                         <div class="version_unidad">5 PTS ADVANCE 16L TA A/AC VE RA-16-2021</div>
--- db/Conexion.php
+++ db/Conexion.php
@@ -1,5 +1,5 @@
 <?php
 $con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "/home/Catalogo/db/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_ssl_set($con,NULL,NULL, "/Catalogo/db/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, "mobilitysolutions-server.mysql.database.azure.com", "btdonyajwn", "h5n9NG>3ktmq$ph", "mobility_solutions", 3306, MYSQLI_CLIENT_SSL);
 ?>
