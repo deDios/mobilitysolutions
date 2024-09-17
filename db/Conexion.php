10b3619c2ed792c89841e0fcbb0e40387e09e516
Pablo de Dios <4855797@gmail.com>
2024-09-16 19:20:50 -0600

    Conexion v34

--- db/Conexion.php
+++ db/Conexion.php
@@ -6,7 +6,6 @@ $con = mysqli_connect("mobilitysolutions-server.mysql.database.azure.com", "btdo
 
 
 $con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "/home/site/wwwroot/Catalogo/db/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($con, "mobilitysolutions-server.mysql.database.azure.com", "btdonyajwn", "h5n9NG>3ktmqph", "mobility_solutions", 3306);
 if (mysqli_connect_errno()){
         die('Error: Falla en la conexion de MySQL'. mysqli_connect_errno());
