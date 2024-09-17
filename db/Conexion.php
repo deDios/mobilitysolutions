7b590aa43c050a78d49f5faaf8236aadbdb880a9
Pablo de Dios <4855797@gmail.com>
2024-09-16 18:19:36 -0600

    Conexion v25

--- db/Conexion.php
+++ db/Conexion.php
@@ -1,7 +1,7 @@
 <?php
-
-$con = mysqli_connect("mobilitysolutions-server.mysql.database.azure.com", "btdonyajwn", "h5n9NG>3ktmqph", "mobility_solutions");
 /*
+$con = mysqli_connect("mobilitysolutions-server.mysql.database.azure.com", "btdonyajwn", "h5n9NG>3ktmqph", "mobility_solutions");
+
 $con = mysqli_init();
 mysqli_ssl_set($con,NULL,NULL, "/home/site/wwwroot/Catalogo/db/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($con, "mobilitysolutions-server.mysql.database.azure.com", "btdonyajwn", "h5n9NG>3ktmqph", "mobility_solutions", 3306, MYSQLI_CLIENT_SSL);
@@ -9,4 +9,9 @@ if (mysqli_connect_errno()){
         die('Error: Falla en la conexion de MySQL'. mysqli_connect_errno());
     }
 */
+
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "/home/site/wwwroot/Catalogo/db/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_real_connect($conn, "mobilitysolutions-server.mysql.database.azure.com", "btdonyajwn", "h5n9NG>3ktmqph", "mobility_solutions", 3306, MYSQLI_CLIENT_SSL);
+
 ?>
