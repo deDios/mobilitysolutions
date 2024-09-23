bf3b4ba8ab13caf66835689842fc19e64c02a69b
Pablo de Dios <4855797@gmail.com>
2024-09-23 03:45:25 -0600

    Agregar pie de página

--- catalogo.php
+++ catalogo.php
@@ -134,6 +134,13 @@
         </div>
     </div>
 
+    <footer>
+        <div class="pie_pag">
+            <p>DERECHOS DE AUTOR &COPY; 2024 - MOBILITY SOLUTIONTS CORPORATIONS</p>
+            <p>Contactanos: contacto@mobilitysolutionscorp.com</p>
+        </div>
+    </footer>
+
    
 
  </body>
