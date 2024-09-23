fee6aae102b1d10d296aa20c232b3ddb5d9ab8ed
Pablo de Dios <4855797@gmail.com>
2024-09-23 11:08:10 -0600

    Agregar bootstrap v6

--- catalogo.php
+++ catalogo.php
@@ -3,7 +3,7 @@
  <html lang="en">
  <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Catálogo</title>
     <link rel="stylesheet" href="CSS/catalogo.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
