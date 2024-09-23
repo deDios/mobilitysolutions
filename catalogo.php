f58e6b8ee370e6df262f4b7238b0937f18d2fbaf
Pablo de Dios <4855797@gmail.com>
2024-09-23 17:53:24 -0600

    Agregar pantalla detalle v20

--- catalogo.php
+++ catalogo.php
@@ -105,39 +105,39 @@
                                                 <div class="Precio"><?php echo "$" . number_format($costo) . " MXN de contado"; ?> </div>
                                                 <div class="Localidad"> <i class="fas fa-location-arrow" ></i> <?php echo " " . $sucursal;?>  </div>
                                             </div>
-                                        </div>
 
-                                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                            <div class="modal-dialog">
-                                                <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Detalle Auto</h1>
-                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                <div class="modal-dialog">
+                                                    <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Detalle Auto</h1>
+                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        <form action="">
+                                                            <table class="table" border = "0">
+                                                                <tr>
+                                                                    <th rowspan="4">
+                                                                        <img src="Imagenes/Catalogo/Auto <?php echo $id;?>/Img01.jpg" alt="Auto 1">
+                                                                    </th>
+                                                                    <th> <?php echo $marca . " " . $nombre;?> </th>
+                                                                    <th> <?php echo $modelo;?> </th>
+                                                                </tr>
+                                                                <tr>
+                                                                    <th> 
+                                                                        Costo:  <?php echo $costo;?> 
+                                                                    </th>
+                                                                </tr>
+                                                            </table>
+                                                        </form>
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                        <button type="button" class="btn btn-primary">Save changes</button>
+                                                    </div>
+                                                    </div>
                                                 </div>
-                                                <div class="modal-body">
-                                                    <form action="">
-                                                        <table class="table" border = "0">
-                                                            <tr>
-                                                                <th rowspan="4">
-                                                                    <img src="Imagenes/Catalogo/Auto <?php echo $id;?>/Img01.jpg" alt="Auto 1">
-                                                                </th>
-                                                                <th> <?php echo $marca . " " . $nombre;?> </th>
-                                                                <th> <?php echo $modelo;?> </th>
-                                                            </tr>
-                                                            <tr>
-                                                                <th> 
-                                                                    Costo:  <?php echo $costo;?> 
-                                                                </th>
-                                                            </tr>
-                                                        </table>
-                                                    </form>
                                                 </div>
-                                                <div class="modal-footer">
-                                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                                    <button type="button" class="btn btn-primary">Save changes</button>
-                                                </div>
-                                                </div>
-                                            </div>
                                         </div>
                                     </a>
                                  
