d9c1194034528ef44c975b97716bb33ec4742354
Pablo de Dios <4855797@gmail.com>
2024-09-23 17:33:32 -0600

    Agregar pantalla detalle v16

--- catalogo.php
+++ catalogo.php
@@ -125,7 +125,27 @@
                                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                         </div>
                                         <div class="modal-body" id="mostrar">
-                                            ...
+                                            <form action="">
+                                                <table class="table" border = "0">
+                                                    <tr>
+                                                        <th rowspan="4">
+                                                            <img src="Imagenes/Catalogo/Auto <?php echo $id;?>/Img01.jpg" alt="Auto 1">
+                                                        </th>
+                                                        <th> <?php echo $marca . " " . $nombre;?> </th>
+                                                        <th> <?php echo $modelo;?> </th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th> 
+                                                            Costo:  <?php echo $costo;?> 
+                                                        </th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>
+                                                            <button type="button" class="btn btn-secondary"> Cerrar </button>
+                                                        </th>
+                                                    </tr>
+                                                </table>
+                                            </form>
                                         </div>
                                         <div class="modal-footer">
                                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
