1f0a007a31cf8c83836b734c02d1b12ca56fe4f5
Pablo de Dios <4855797@gmail.com>
2024-09-23 14:54:12 -0600

    Agregar bootstrap v48

--- detalle.php
+++ detalle.php
@@ -3,49 +3,54 @@
 $cod=$_REQUEST['cod'];
 
 $inc = include "db/Conexion.php"; 
-$query = '  select 
-                        auto.id,
-                        m_auto.auto as nombre, 
-                        modelo.nombre as modelo, 
-                        marca.nombre as marca, 
-                        auto.mensualidad, 
-                        auto.costo, 
-                        sucursal.nombre as sucursal, 
-                        auto.img1, 
-                        auto.img2, 
-                        auto.img3, 
-                        auto.img4, 
-                        auto.img5, 
-                        auto.img6, 
-                        auto.color, 
-                        auto.transmision, 
-                        auto.interior, 
-                        auto.kilometraje, 
-                        auto.combustible, 
-                        auto.cilindros, 
-                        auto.eje, 
-                        auto.estatus, 
-                        auto.created_at, 
-                        auto.updated_at 
-                    FROM mobility_solutions.tmx_auto as auto
-                    left join mobility_solutions.tmx_sucursal as sucursal on auto.sucursal = sucursal.id 
-                    left join mobility_solutions.tmx_estatus as estatus on auto.estatus = estatus.id
-                    left join mobility_solutions.tmx_modelo as modelo on auto.modelo = modelo.id 
-                    left join mobility_solutions.tmx_marca as marca on auto.marca = marca.id
-                    left join mobility_solutions.tmx_marca_auto as m_auto on auto.nombre = m_auto.id;
-                    where auto.id = $cod';
+$query = 'select 
+                auto.id,
+                m_auto.auto as nombre, 
+                modelo.nombre as modelo, 
+                marca.nombre as marca, 
+                auto.mensualidad, 
+                auto.costo, 
+                sucursal.nombre as sucursal, 
+                auto.img1, 
+                auto.img2, 
+                auto.img3, 
+                auto.img4, 
+                auto.img5, 
+                auto.img6, 
+                auto.color, 
+                auto.transmision, 
+                auto.interior, 
+                auto.kilometraje, 
+                auto.combustible, 
+                auto.cilindros, 
+                auto.eje, 
+                auto.estatus, 
+                auto.created_at, 
+                auto.updated_at 
+            FROM mobility_solutions.tmx_auto as auto
+            left join mobility_solutions.tmx_sucursal as sucursal on auto.sucursal = sucursal.id 
+            left join mobility_solutions.tmx_estatus as estatus on auto.estatus = estatus.id
+            left join mobility_solutions.tmx_modelo as modelo on auto.modelo = modelo.id 
+            left join mobility_solutions.tmx_marca as marca on auto.marca = marca.id
+            left join mobility_solutions.tmx_marca_auto as m_auto on auto.nombre = m_auto.id;
+            where auto.id = $cod';
+
         $result = mysqli_query($con,$query); 
         if ($result){ 
-            foreach ($result as $row){
-                $lista[] = $row;
+            while($row = mysqli_fetch_assoc($result)){
+                                $id = $row['id'];
+                                $nombre = $row['nombre'];
+                                $modelo = $row['modelo'];
+                                $marca = $row['marca'];
+                                $mensualidad = $row['mensualidad'];
+                                $costo = $row['costo'];
+                                $sucursal = $row['sucursal'];
             }
         }
         else{
             echo "Falla en conexión";
         }
-
-        $nombre = $lista['nombre'];
-
+        
 echo $cod;
 echo $nombre;
 
