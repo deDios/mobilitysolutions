559e0dbf8985298453b096e5ec0014655a89bbe5
Pablo de Dios <4855797@gmail.com>
2024-09-23 23:37:23 -0600

    Agregar pantalla detalle v60

--- catalogo.php
+++ catalogo.php
@@ -121,7 +121,8 @@
                                                         <h1 class="modal-title fs-5" id="exampleModalLabel">Detalle Auto</h1>
                                                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                     </div>
-                                                    <div class="modal-body" id="body_detalle">
+                                                    <div class="modal-body" id="body_detalle">      
+                                                        <?php echo $row[0]; ?>
                                                         <?php echo $id; ?>    
                                                         <?php echo $nombre; ?>
                                                     </div>
