2222b7bd455ff573515de161552ed23ae04b5cb9
Pablo de Dios <4855797@gmail.com>
2024-09-24 14:21:53 -0600

    Agregar pantalla detalle v169

--- detalle.php
+++ detalle.php
@@ -62,7 +62,6 @@ $query = 'select
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Detalle del auto</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="CSS/detalle.css">
 
  </head>
@@ -186,7 +185,5 @@ $query = 'select
         </div>
     </div>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>
