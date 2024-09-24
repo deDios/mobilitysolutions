696968fb5829ded267fb954487f8417cb64a41b0
Pablo de Dios <4855797@gmail.com>
2024-09-23 22:25:10 -0600

    Agregar pantalla detalle v57

--- prueba.php
+++ prueba.php
@@ -14,16 +14,16 @@ include "Conexion.php";
 <div id = "body_detalle"> Este es mi texto </div>
 
 <script src="js/jquery-3.7.1.js">
-        var div_detalle = document.getElementById("body_detalle");
+        var div_detalle = document.getElementById('body_detalle');
         function abrir_detalle(){
                 var xmlhttp = new XMLHttpRequest();
-                xmlhttp.open("GET","detalle.php?cod=5",true);
+                xmlhttp.open('GET','detalle.php?cod=5',true);
                 xmlhttp.onload = function (){
                     if (xmlhttp.status==200){
                         div_detalle.innerHTML = xmlhttp.responseText;
                     }
                     else{
-                        div_detalle.innerHTML = "Cargando....";
+                        div_detalle.innerHTML = 'Cargando....';
                     }       
             }
             xmlhttp.send();   
