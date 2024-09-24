995e98309a3abf02e34cb96e60b02bc643c6a9b2
Pablo de Dios <4855797@gmail.com>
2024-09-24 17:45:57 -0600

    Agregar pantalla detalle v205

--- detalles.php
+++ detalles.php
@@ -171,6 +171,34 @@ $query = 'select
                         <span class="badge bg-primary rounded-pill">1</span>
                     </li>
                 </ul>
+                <ul class="list-group">
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        Interior
+                        <span class="badge bg-primary rounded-pill"><?php echo $interior;?></span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        A second list item
+                        <span class="badge bg-primary rounded-pill">2</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        A third list item
+                        <span class="badge bg-primary rounded-pill">1</span>
+                    </li>
+                </ul>
+                <ul class="list-group">
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        Interior
+                        <span class="badge bg-primary rounded-pill"><?php echo $interior;?></span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        A second list item
+                        <span class="badge bg-primary rounded-pill">2</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        A third list item
+                        <span class="badge bg-primary rounded-pill">1</span>
+                    </li>
+                </ul>
             </div>
 
         </div>
