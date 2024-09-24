b7515b1deea3735e7f176cd93911f5ef957c41a2
Pablo de Dios <4855797@gmail.com>
2024-09-24 10:33:04 -0600

    Agregar pantalla detalle v104

--- detalle.php
+++ detalle.php
@@ -55,14 +55,31 @@ echo $cod;
 echo $nombre;
 ?>
 
-<html lang="en">
-<head>
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>Mobility solutions</title>
-    <link rel="stylesheet" href="CSS/detalle.css">
-</head>
-<body>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detalle del auto</title>
+    <link rel="stylesheet" href="CSS/catalogo.css">
+ </head>
+ <body>
+    <header>
+        <a href="index.php" class="logo" title="Home">
+            <img src="logo_MSC.png" alt="Logo de la compañia">
+            <h2 class="Nombre de la empresa">
+                Mobility Solutions Corporation
+            </h2>
+        </a>
+        <nav>
+            <a href="index.php" class="nav-link" title="Home">Inicio</a>
+            <a href="catalogo.php" class="nav-link" title="Home">Catálogo</a>
+            <a href="" class="nav-link">Cotizador</a>
+            <a href="" class="nav-link">Sobre nosotros</a>
+            <a href="" class="nav-link">Contacto</a>
+        </nav>
+    </header>
+
     <form action="">
         <table class="table" border = "0">
             <tr>
