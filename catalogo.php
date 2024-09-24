a1ee564f58c2b123661415a3e894153836c1b716
Pablo de Dios <4855797@gmail.com>
2024-09-24 10:17:08 -0600

    Agregar pantalla detalle v103

--- catalogo.php
+++ catalogo.php
@@ -1,16 +1,11 @@
 
  <!DOCTYPE html>
-
-
  <html lang="en">
  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Catálogo</title>
     <link rel="stylesheet" href="CSS/catalogo.css">
-    <!-- Enlaza Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
  </head>
  <body>
     <header>
@@ -114,50 +109,11 @@
             ?>
         </div>
     </div>
-
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Detalle Auto</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body" id="body_detalle">      
-                ...
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Cotizar</button>
-            </div>
-            </div>
-        </div>
-    </div>  
-                                                      
-
-    <script src="js/jquery-3.7.1.js"> 
-        
-        var div_detalle = document.getElementById("body_detalle");
-        
-        function abrir_detalle(cod){
-            
-            var xmlhttp = new XMLHttpRequest();  
-            xmlhttp.open("GET","detalle.php?cod="+cod,true);  
-                xmlhttp.onload = function (){
-                    if (xmlhttp.status==200){
-                        document.querySelector("body_detalle").innerHTML = xmlhttp.responseText;
-                        div_detalle.innerHTML= xmlhttp.responseText;
-                    }
-                    else{
-                        div_detalle.innerHTML = "Cargando....";
-                    }       
-            }  
-            xmlhttp.send(); 
-        }
-
+                                                       
+    <script>        
         function detalle (cod){
             location.href="detalle.php?cod="+cod;
         }
-
     </script>
 
     <footer>
@@ -166,10 +122,6 @@
             <p>Contactanos: contacto@mobilitysolutionscorp.com</p>
         </div>
     </footer>
-
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-
 
  </body>
  </html>
