037ee7376b7b3f549d85a7f2428543ab8d7f5069
Pablo de Dios <4855797@gmail.com>
2024-09-24 11:10:11 -0600

    Agregar pantalla detalle v109

--- detalle.php
+++ detalle.php
@@ -80,27 +80,45 @@ echo $nombre;
         </nav>
     </header>
 
-    <form action="">
-        <table class="table" border = "0">
-            <tr>
-                <th rowspan="4">
-                    <img src="Imagenes/Catalogo/Auto <?php echo $id;?>/Img01.jpg" alt="Auto 1">
-                </th>
-                <th> <?php echo $marca . " " . $nombre;?> </th>
-                <th> <?php echo $modelo;?> </th>
-            </tr>
-            <tr>
-                <th> 
-                    Costo:  <?php echo $costo;?> 
-                </th>
-            </tr>
-            <tr>
-                <th>
-                    <button type="button" class="btn btn-secondary"> Cerrar </button>
-                </th>
-            </tr>
-        </table>
-    </form>
+    <div class="contenedor">
+        <div class="contenedor_carr">
+        <!-- Titulo de carrusel -->
+            <div class="titulo_carr">
+                Titulo
+            </div>
+        <!-- Carrusel auto seleccionado -->
+            <div class="carrusel_carr">
+                <img src="Imagenes/Catalogo/Auto <?php echo $id;?>/Img01.jpg" alt="Auto 1">
+            </div>
+        <!-- Div detalle de auto -->
+            <div class="detalle_carr">
+            <form action="">
+                    <table class="table" border = "0">
+                        <tr>
+                            <th rowspan="4">
+                            </th>
+                            <th> <?php echo $marca . " " . $nombre;?> </th>
+                            <th> <?php echo $modelo;?> </th>
+                        </tr>
+                        <tr>
+                            <th> 
+                                Costo:  <?php echo $costo;?> 
+                            </th>
+                        </tr>
+                        <tr>
+                            <th>
+                                <button type="button" class="btn btn-secondary"> Cerrar </button>
+                            </th>
+                        </tr>
+                    </table>
+                </form>
+            </div>
+        </div>
+        <div class="cotizador">
+
+        </div>
+    </div>
+
 
 
     <footer>
