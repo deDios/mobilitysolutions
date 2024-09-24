f65fd099cc030ba381d82ffdcd256ad043454304
Pablo de Dios <4855797@gmail.com>
2024-09-23 20:49:16 -0600

    Agregar pantalla detalle v40

--- catalogo.php
+++ catalogo.php
@@ -11,8 +11,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
  </head>
  <body>
-
- 
     <header>
         <a href="index.php" class="logo" title="Home">
             <img src="logo_MSC.png" alt="Logo de la compañia">
@@ -134,7 +132,7 @@
                                                 </div>
                                             </div>            
 
-    <script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
         var div_detalle = document.getElementById("body_detalle");
         function abrir_detalle(cod){
                 var xmlhttp;
