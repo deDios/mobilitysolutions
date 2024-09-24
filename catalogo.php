fa900453dbe60c519f30608272ed823aacf1cad4
Pablo de Dios <4855797@gmail.com>
2024-09-23 21:34:56 -0600

    Agregar pantalla detalle v46

--- catalogo.php
+++ catalogo.php
@@ -132,7 +132,7 @@
                                                 </div>
                                             </div>            
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+    <script src="js/jquery-3.2.1.min.js">
         var div_detalle = document.getElementById("body_detalle");
         function abrir_detalle(cod){
                 var xmlhttp; 
