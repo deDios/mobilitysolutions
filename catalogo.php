b3811aeee869d1c93dcd7a2963a1a4d738822c8c
Pablo de Dios <4855797@gmail.com>
2024-09-23 22:11:18 -0600

    Agregar pantalla detalle v53

--- catalogo.php
+++ catalogo.php
@@ -122,7 +122,7 @@
                                                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                     </div>
                                                     <div class="modal-body" id="body_detalle">
-                                                        ...
+                                                        abrir_detalle(5);
                                                     </div>
                                                     <div class="modal-footer">
                                                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
