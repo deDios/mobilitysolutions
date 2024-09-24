c59fa66b86c3b03a168ded5cc87116dabb74f258
Pablo de Dios <4855797@gmail.com>
2024-09-24 12:18:21 -0600

    Agregar pantalla detalle v123

--- detalle.php
+++ detalle.php
@@ -122,7 +122,7 @@ echo $nombre;
                         </div>
 
                         <div class="item">
-                            <img src="https://ugc.kn3.net/i/origin/http://thisiscolossal.com/wp-content/uploads/2011/02/anthony-2-600x424.jpg" alt="fotografia" width="600" height="424"> 
+                            <img src="Imagenes/Catalogo/Auto <?php echo $id;?>/Img01.jpg" alt="fotografia" width="600" height="424"> 
                             <div class="carousel-caption d-none d-md-block">
                                 <h3> Xataka Fotografía</h3>
                                 <p>Concurso</p>
@@ -130,7 +130,7 @@ echo $nombre;
                         </div>
 
                         <div class="item">
-                            <img src="https://mir-s3-cdn-cf.behance.net/project_modules/1400/e86e8b59124321.5a15e4ccd8622.jpg" alt="pintura" width="600" height="424">
+                            <img src="Imagenes/Catalogo/Auto <?php echo $id;?>/Img01.jpg" alt="pintura" width="600" height="424">
                             <div class="carousel-caption d-none d-md-block">
                                 <h3>DoArt Pintura</h3>
                                 <p>Curso</p>
@@ -138,7 +138,7 @@ echo $nombre;
                         </div>
 
                         <div class="item">
-                            <img src="https://cdn.dribbble.com/users/324248/screenshots/3978513/planet2_800.jpg" alt="diseno" width="600" height="424"> 
+                            <img src="Imagenes/Catalogo/Auto <?php echo $id;?>/Img01.jpg" alt="diseno" width="600" height="424"> 
                             <div class="carousel-caption d-none d-md-block">
                                 <h3>UX UI Diseño Gráfico</h3>
                                 <p>Curso</p>
@@ -146,15 +146,15 @@ echo $nombre;
                         </div>
 
 
-                    <a class="left carousel-control" href="#theCarousel" role="button" data-slide="prev">
-                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                        <span class="sr-only">anterior</span>
-                    </a>
-                    <a class="right carousel-control" href="#theCarousel" role="button" data-slide="next">
-                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                        <span class="sr-only">siguiente</span>
-                    </a>
-                </div>
+                        <a class="left carousel-control" href="#theCarousel" role="button" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                            <span class="sr-only">anterior</span>
+                        </a>
+                        <a class="right carousel-control" href="#theCarousel" role="button" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                            <span class="sr-only">siguiente</span>
+                        </a>
+                    </div>
             </div>
 
             <!-- Div detalle de auto -->
