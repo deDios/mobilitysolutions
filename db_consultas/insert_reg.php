c6e52b219378ce5dbefe5de9694b9fc3209cfc3b
Pablo de Dios <4855797@gmail.com>
2024-09-25 12:21:31 -0600

    Agregar pantalla detalle v273

--- db_consultas/insert_reg.php
+++ db_consultas/insert_reg.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href=*https://cdn_jsdelivr.net/npm/bootstrap®5.2.2/dist/css/bootstrap.min.css* rel="stylesheet"
+    integrity="sha384-Zenh87qX5JnK2JL0vWaBCk2rdkQ2Bzep5IDxbcnCeu0xjzrPF/et3URy9BvIWTR1* crossorigin="anonymous">
+    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
