7c1521995c56561df13078333094a46f93a9c2ce
Pablo de Dios <4855797@gmail.com>
2024-09-25 16:11:37 -0600

    Agregar pantalla detalle v305

--- get_marca.php
+++ get_marca.php
@@ -23,3 +23,14 @@ $inc = include "db/Conexion.php";
         echo $cadena."</select>";
     }  
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>get_marca</title>
+</head>
+<body>
+    
+</body>
+</html>
