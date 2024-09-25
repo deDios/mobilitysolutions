8eed59cf27a674e60288d92abea60128c4422e6a
Pablo de Dios <4855797@gmail.com>
2024-09-24 19:31:37 -0600

    Agregar pantalla detalle v238

--- editar_cat.php
+++ editar_cat.php
@@ -1,3 +1,8 @@
+<?php
+$cod=$_REQUEST['id'];
+
+?>
+
 <!DOCTYPE html>
  <html lang="en">
  <head>
@@ -9,7 +14,6 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
  </head>
  <body>
     <header>
