28fd8afd91a2b4ba8c9ff1307c0cca3057243659
Pablo de Dios <4855797@gmail.com>
2024-09-24 19:28:43 -0600

    Agregar pantalla detalle v237

---
+++ editar_cat.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Editar catálogo</title>
+    <link rel="stylesheet" href="CSS/editar_cat.css">
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+ </head>
+ <body>
+    <header>
+        <a href="index.php" class="logo" title="Home">
+            <img src="logo_MSC.png" alt="Logo de la compañia">
+            <h2 class="Nombre de la empresa">
+                Mobility Solutions Corporation
+            </h2>
+        </a>
+        <nav>
+            <a href="index.php" class="nav-link" title="Home">Inicio</a>
+            <a href="catalogo.php" class="nav-link" title="Home">Catálogo</a>
+            <a href="" class="nav-link">Cotizador</a>
+            <a href="" class="nav-link">Sobre nosotros</a>
+            <a href="" class="nav-link">Contacto</a>
+        </nav>
+    </header>
+
+</body>
+</html>
