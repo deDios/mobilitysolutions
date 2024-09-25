1dfe53213c1fb9240a5dae063d883c37d6872e15
Pablo de Dios <4855797@gmail.com>
2024-09-25 12:19:13 -0600

    Agregar pantalla detalle v272

--- db_consultas/insert_reg.php
+++ db_consultas/insert_reg.php
@@ -18,17 +18,17 @@
             <div class="col">
                 <form action="">
                     <div class="col mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Marca</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <label for="InputMarca" class="form-label">Marca</label>
+                        <input type="text" class="form-control" id="InputMarca" aria-describedby="emailHelp">
                         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                     </div>
-                    <div class="col-6 mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Nombre</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1">
+                    <div class="col-6">
+                        <label for="InputNombre" class="form-label">Nombre</label>
+                        <input type="text" class="form-control" id="InputNombre">
                     </div>
-                    <div class="col-6 mb-3 form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">Modelo</label>
+                    <div class="col-6">
+                        <label for="InputModelo" class="form-label">Modelo</label>
+                        <input type="text" class="form-control" id="InputModelo">
                     </div>
                     <div class="col mb-3 form-check">
                         <input type="checkbox" class="form-check-input" id="exampleCheck1">
