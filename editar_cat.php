f891260e0a306dae66f87273e94dd0d3fc00ffa9
Pablo de Dios <4855797@gmail.com>
2024-09-24 20:18:24 -0600

    Agregar pantalla detalle v249

--- editar_cat.php
+++ editar_cat.php
@@ -64,27 +64,89 @@ $cod=$_REQUEST['id'];
                     </tr>
                 </thead>
                 <tbody>
+                <?php 
+                $inc = include "db/Conexion.php";    
+                    if ($inc){
+                        $query = '  select 
+                                        auto.id,
+                                        m_auto.auto as nombre, 
+                                        modelo.nombre as modelo, 
+                                        marca.nombre as marca, 
+                                        auto.mensualidad, 
+                                        auto.costo, 
+                                        sucursal.nombre as sucursal, 
+                                        auto.img1, 
+                                        auto.img2, 
+                                        auto.img3, 
+                                        auto.img4, 
+                                        auto.img5, 
+                                        auto.img6, 
+                                        auto.color, 
+                                        auto.transmision, 
+                                        auto.interior, 
+                                        auto.kilometraje, 
+                                        auto.combustible, 
+                                        auto.cilindros, 
+                                        auto.eje, 
+                                        auto.estatus, 
+                                        auto.created_at, 
+                                        auto.updated_at 
+                                    FROM mobility_solutions.tmx_auto as auto
+                                    left join mobility_solutions.tmx_sucursal as sucursal on auto.sucursal = sucursal.id 
+                                    left join mobility_solutions.tmx_estatus as estatus on auto.estatus = estatus.id
+                                    left join mobility_solutions.tmx_modelo as modelo on auto.modelo = modelo.id 
+                                    left join mobility_solutions.tmx_marca as marca on auto.marca = marca.id
+                                    left join mobility_solutions.tmx_marca_auto as m_auto on auto.nombre = m_auto.id;';
+                        $result = mysqli_query($con,$query);  
+                        if ($result){         
+                            while($row = mysqli_fetch_assoc($result)){
+                                $id = $row['id'];
+                                $nombre = $row['nombre'];
+                                $modelo = $row['modelo'];
+                                $marca = $row['marca'];
+                                $mensualidad = $row['mensualidad'];
+                                $costo = $row['costo'];
+                                $sucursal = $row['sucursal'];
+                                $color = $row['color'];
+                                $interior = $row['interior'];
+                                $combustible = $row['combustible'];
+                                $cilindros = $row['cilindros'];
+                                $transmision = $row['transmision'];
+                                $kilometraje = $row['kilometraje'];
+                                $eje = $row['eje'];
+                                $estatus = $row['estatus'];
+                                $created_at = $row['created_at'];
+                                $updated_at = $row['updated_at'];
+                ?> 
                     <tr>
-                        <th class="">1</th>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
+                        <th class=""><?php echo $id;?></th>
+                        <td><?php echo $marca;?></td>
+                        <td><?php echo $nombre;?></td>
+                        <td><?php echo $modelo;?></td>
+                        <td><?php echo $mensualidad;?></td>
+                        <td><?php echo $costo;?></td>
+                        <td><?php echo $sucursal;?></td>
+                        <td><?php echo $color;?></td>
+                        <td><?php echo $transmision;?></td>
+                        <td><?php echo $interior;?></td>
+                        <td><?php echo $kilometraje;?></td>
+                        <td><?php echo $combustible;?></td>
+                        <td><?php echo $cilindros;?></td>
+                        <td><?php echo $eje;?></td>
+                        <td><?php echo $estatus;?></td>
+                        <td><?php echo $created_at;?></td>
+                        <td><?php echo $updated_at;?></td>
                         <td><button class="btn btn-warning">Editar</button></td>
                         <td><button class="btn btn-danger">Eliminar</button> </td>
                     </tr>
+                <?php
+                            }
+                        } else{
+                            echo "Hubo un error en la consulta";
+                        }
+                        mysqli_free_result($result);                  
+                    }
+                ?>
                 </tbody>
             </table>
         </div>
