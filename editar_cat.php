c2aa4ff1ce37fa85a59a23c27e08187fb2589e7a
Pablo de Dios <4855797@gmail.com>
2024-09-24 20:29:22 -0600

    Agregar pantalla detalle v253

--- editar_cat.php
+++ editar_cat.php
@@ -14,6 +14,9 @@ $cod=$_REQUEST['id'];
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css"></script>
+    <script src="https://cdn.datatables.net/2.1.7/css/dataTables.bootstrap5.css"></script>
+    
  </head>
  <body>
     <header>
@@ -155,5 +158,6 @@ $cod=$_REQUEST['id'];
 <script>
     new DataTable('mytable');
 </script>
+
 </body>
 </html>
