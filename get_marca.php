8e22b2bf061b826abf3cfbfbdff5707f584741e8
Pablo de Dios <4855797@gmail.com>
2024-09-25 15:42:26 -0600

    Agregar pantalla detalle v302

--- get_marca.php
+++ get_marca.php
@@ -13,10 +13,15 @@ $inc = include "db/Conexion.php";
     
         $cadena = " <label for='InputNombre' class='form-label'>Vehiculo</label>
                     <select id='InputNombre' class='form-select' aria-label='Default select example' name='InputNombre'>";
+        if ($result){ 
+            while ($ver=mysqli_fetch_row($result)){
+                $cadena=$cadena. '<opcion value'.$ver['id'].'>'.$ver['nombre'].'</option>';
+            }
+        }else{
+            echo "Hubo un error en la consulta";
+            }
 
-        while ($ver=mysqli_fetch_row($result)){
-            $cadena=$cadena. '<opcion value'.$ver['id'].'>'.$ver['nombre'].'</option>';
-        }
         echo $cadena."</select>";
     }
+    
 ?>
