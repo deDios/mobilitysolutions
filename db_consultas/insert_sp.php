dcfe7943779dcdd335e315d2927609d33d8b60ae
Pablo de Dios <4855797@gmail.com>
2024-09-26 11:07:16 -0600

    Agregar pantalla detalle v438

--- db_consultas/insert_sp.php
+++ db_consultas/insert_sp.php
@@ -33,7 +33,13 @@ $inc = include "db/Conexion.php";
 $query = 'insert into mobility_solutions.tmx_auto (nombre, modelo, marca, mensualidad, costo, sucursal, img1, img2, img3, img4, img5, img6, color, transmision, interior, kilometraje, combustible, cilindros, eje, estatus, created_at, pasajeros, propietarios) values
 ('.$auto.','.$modelo.','.$marca.','.$mensualidad.','.$costo.','.$sucursal.','.$img1.', '.$img2.', '.$img3.', '.$img4.', '.$img5.', '.$img6.','.$color_valor.', '.$transmision_valor.','.$interior_valor.','.$kilometraje.','.$combustible_valor.','.$cilindros.','.$eje_valor.',2,NULL,'.$pasajeros.','.$propietarios.');';
         
-    $result = mysqli_query($con,$query); 
+$result = mysqli_query($con,$query); 
+if ($result){ 
+    echo "Exito en conexión";
+}
+else{
+    echo "Falla en conexión";
+}
 
 
 ?>
@@ -43,6 +49,9 @@ $query = 'insert into mobility_solutions.tmx_auto (nombre, modelo, marca, mensua
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>insert</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
     
