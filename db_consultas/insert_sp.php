a9607628bd51571513a88908c2a1498f639bdfb7
Pablo de Dios <4855797@gmail.com>
2024-09-26 03:11:54 -0600

    Agregar pantalla detalle v427

--- db_consultas/insert_sp.php
+++ db_consultas/insert_sp.php
@@ -1,7 +1,35 @@
 <?php
 $sucursal = $_POST['InputSucursal'];
+$marca = $_POST['InputMarca'];
+$auto = $_POST['InputNombre'];
+$modelo = $_POST['InputModelo'];
+$mensualidad = $_POST['InputMensualidad'];
+$costo = $_POST['InputCosto'];
+$color = $_POST['InputColor'];
+$transmision = $_POST['InputTransmision'];
+$interior = $_POST['InputInterior'];
+$kilometraje = $_POST['InputKilometraje'];
+$combustible = $_POST['InputCombustible'];
+$cilindros = $_POST['InputCilindros'];
+$eje = $_POST['InputEje'];
+$pasajeros = $_POST['InputPasajeros'];
+$propietarios = $_POST['InputPropietarios'];
 
-echo $sucursal;
+echo "sucursal" . $sucursal;
+echo "marca" . $marca;
+echo "auto" . $auto;
+echo "modelo" . $modelo;
+echo "mensualidad" . $mensualidad;
+echo "costo" . $costo;
+echo "color" . $color;
+echo "transmision" . $transmision;
+echo "interior" . $interior;
+echo "kilometraje" . $kilometraje;
+echo "combustible" . $combustible;
+echo "cilindros" . $cilindros;
+echo "eje" . $eje;
+echo "pasajeros" . $pasajeros;
+echo "propietarios" . $propietarios;
 ?>
 <!DOCTYPE html>
 <html lang="en">
