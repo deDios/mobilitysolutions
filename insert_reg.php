6a2b315fd912db8656cd6e651dfe892d367f91c6
Pablo de Dios <4855797@gmail.com>
2024-09-26 00:21:47 -0600

    Agregar pantalla detalle v394

--- insert_reg.php
+++ insert_reg.php
@@ -100,11 +100,12 @@
 
                     </div>
                     
-                    <div class="input-group mb-3">
+                    <div class="input-group mb-3 col-6 mt-5">
                         <span class="input-group-text">$</span>
                             <input id="InputMensualidad" type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
                         <span class="input-group-text">MX/mensuales</span>
                     </div>
+
                     <div class="col mb-3 form-check">
                         <input type="checkbox" class="form-check-input" id="exampleCheck1">
                         <label class="form-check-label" for="exampleCheck1">Costo de contado</label>
