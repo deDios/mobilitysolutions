ebf3df8312497cbc6eb623c6ff3e5762f4b04174
Pablo de Dios <4855797@gmail.com>
2024-09-26 00:33:23 -0600

    Agregar pantalla detalle v399

--- insert_reg.php
+++ insert_reg.php
@@ -100,7 +100,7 @@
 
                     </div>
                     
-                    <div class="input-group mb-3 col-6 mt-5">
+                    <div class="input-group mb-3 col-6 mt-2">
                         <span class="input-group-text">$</span>
                             <input id="InputMensualidad" type="text" class="form-control" aria-label="Amount (to the nearest dollar)" required>
                         <span class="input-group-text">MX/mensuales</span>
