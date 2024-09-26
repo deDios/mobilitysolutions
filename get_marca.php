e806dcbc1bc2ff9a35b59660b4bc8948084179c7
Pablo de Dios <4855797@gmail.com>
2024-09-25 18:13:23 -0600

    Agregar pantalla detalle v323

--- get_marca.php
+++ get_marca.php
@@ -6,26 +6,27 @@ $Marca=$_POST['Marca'];
                     <option value='0'>Menu recuperado</option>
                     <option value='1'>Opcion 1</option>
                 </select>";
-echo $cadena;
 
-    $inc = include "db/Conexion.php"; 
-    $query = 'select 
+$inc = include "db/Conexion.php"; 
+    $query = "select 
                     id,
                     marca,
                     nombre
                 FROM mobility_solutions.tmx_marca_auto
-                where marca = '. $Marca .';';
+                where marca = 4;";
     
-            $result = mysqli_query($con,$query); 
+    $result = mysqli_query($con,$query); 
             if ($result){ 
                 while($row = mysqli_fetch_assoc($result)){
                                     $id = $row['id'];
                                     $nombre = $row['nombre'];
+
                     $cadena = $cadena . '<opcion value'. $id .'>'. $nombre .'</option>';
                 }
             }
             else{
                 echo "Falla en conexión";
             }
-            echo $cadena . "</select>";
+            $cadena = $cadena . "</select>"; 
+            echo $cadena;
 ?>
