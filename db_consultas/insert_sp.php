82dfa0b455cb4f0a44d219891ab129f5816705fb
Pablo de Dios <4855797@gmail.com>
2024-09-26 02:54:19 -0600

    Agregar pantalla detalle v425

--- db_consultas/insert_sp.php
+++ db_consultas/insert_sp.php
@@ -1,6 +1,14 @@
 <?php
-$Marca=$_POST['InputMarca'];
-
-echo $Marca;
 echo "Hola";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>insert</title>
+</head>
+<body>
+    
+</body>
+</html>
