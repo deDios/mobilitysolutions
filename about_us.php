c5b12775e5d1b3ea0de248f3edacddbf2c769b49
Pablo de Dios <4855797@gmail.com>
2024-09-27 03:15:44 -0600

    Agregar imagenes v53

--- about_us.php
+++ about_us.php
@@ -70,7 +70,7 @@
 
 <main>
 
-  <section class="py-8 text-center container">
+  <section class="py-2 text-center container">
     <div class="row py-lg-5">
       <div class="col-lg-6 col-md-8 mx-auto">
         <h1 class="fw-light">Sobre nosotros</h1>
