704e6f0d3797a78c58d974db5b4b40e1e7f207ec
Pablo de Dios <4855797@gmail.com>
2024-09-27 10:30:28 -0600

    Agregar imagenes v77

--- contact.php
+++ contact.php
@@ -35,38 +35,33 @@
         </div>
       </div>
   </header>
-  <nav class="navbar navbar-expand-lg navbar-dark mx-background-top-linear">
-    <div class="container">
-      <a class="navbar-brand" rel="nofollow" target="_blank" href="https://mobilitysolutionscorp.com"> Mobility Solutions Corporation</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
 
-      <ul class="navbar-nav ml-auto">
+<nav class="navbar navbar-expand-sm navbar-dark">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<h1>Hello, world!</h1>
 
-        <li class="nav-item">
-        <a class="nav-link" href="https://mobilitysolutionscorp.com">Inicio
-            <span class="sr-only">(current)</span>
-        </a>
-        </li>
-
-        <li class="nav-item">
-        <a class="nav-link" href="https://mobilitysolutionscorp.com/catalogo.php">Catálogo</a>
-        </li>
-
-        <li class="nav-item">
-        <a class="nav-link" href="https://mobilitysolutionscorp.com/about_us.php">Nosotros</a>
-        </li>
-
-        <li class="nav-item active">
-        <a class="nav-link" href="https://mobilitysolutionscorp.com/contact.php">Contacto</a>
-        </li>
-
-        </ul>
-      </div>
-    </div>
-  </nav>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </div>
 
 
