77c717d727fb0a2258d3d0457f24269bcb41c8ce
Pablo de Dios <4855797@gmail.com>
2024-09-27 00:55:21 -0600

    Agregar imagenes v18

--- index.php
+++ index.php
@@ -7,8 +7,7 @@ include "Conexion.php";
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.84.0">
+    <meta name="author" content="Pablo de Dios">
     <title>Mobility Solutions Corporation</title>
     <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/headers/">
     <!-- Bootstrap core CSS -->
