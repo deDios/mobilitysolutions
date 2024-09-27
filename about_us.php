e7ae5aa243702d499a3b2b3492cfafa91b026f63
Pablo de Dios <4855797@gmail.com>
2024-09-27 03:18:02 -0600

    Agregar imagenes v54

--- about_us.php
+++ about_us.php
@@ -148,10 +148,10 @@
 <footer class="text-muted py-5">
   <div class="container">
     <p class="float-end mb-1">
-      <a href="#">Back to top</a>
+      <a href="#">Regresa al inicio</a>
     </p>
-    <p class="mb-1">Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
-    <p class="mb-0">New to Bootstrap? <a href="/">Visit the homepage</a> or read our <a href="../getting-started/introduction/">getting started guide</a>.</p>
+    <p class="mb-1">DERECHOS DE AUTOR &COPY; 2024 - MOBILITY SOLUTIONTS CORPORATIONS</p>
+    <p class="mb-0">Contactanos: contacto@mobilitysolutionscorp.com</p>
   </div>
 </footer>
 
