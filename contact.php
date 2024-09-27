fcab0fa1293af7676153a22a794613bd4464a196
Pablo de Dios <4855797@gmail.com>
2024-09-27 10:14:31 -0600

    Agregar imagenes v73

--- contact.php
+++ contact.php
@@ -32,38 +32,40 @@
         </div>
       </div>
   </header>
-  <nav class="navbar navbar-expand-lg navbar-dark mx-background-top-linear">
-    <div class="container">
-      <a class="navbar-brand" rel="nofollow" target="_blank" href="https://mobilitysolutionscorp.com"> Mobility Solutions Corporation</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
+  <nav class="navbar navbar-expand-lg navbar-light bg-info">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-      <ul class="navbar-nav ml-auto">
-
-        <li class="nav-item">
-        <a class="nav-link" href="https://mobilitysolutionscorp.com">Inicio
-            <span class="sr-only">(current)</span>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Next Steps
         </a>
-        </li>
-
-        <li class="nav-item">
-        <a class="nav-link" href="https://mobilitysolutionscorp.com/catalogo.php">Catálogo</a>
-        </li>
-
-        <li class="nav-item">
-        <a class="nav-link" href="https://mobilitysolutionscorp.com/about_us.php">Nosotros</a>
-        </li>
-
-        <li class="nav-item active">
-        <a class="nav-link" href="https://mobilitysolutionscorp.com/contact.php">Contacto</a>
-        </li>
-
-        </ul>
-      </div>
-    </div>
-  </nav>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Contact us</a>
+          <a class="dropdown-item" href="#">Subscribe to the blog</a>
+          <a class="dropdown-item" href="#">Sign up for our newsletter</a>
+        </div>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
 </div>
 
 
