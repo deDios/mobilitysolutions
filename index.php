e30e76bf56149ea432b86b50c26a9cc1b8cf8fee
Pablo de Dios <4855797@gmail.com>
2024-09-27 00:54:12 -0600

    Agregar imagenes v17

--- index.php
+++ index.php
@@ -4,10 +4,31 @@ include "Conexion.php";
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>Mobility solutions</title>
-    <link rel="stylesheet" href="CSS/estilos.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.84.0">
+    <title>Mobility Solutions Corporation</title>
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/headers/">
+    <!-- Bootstrap core CSS -->
+    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>  
+    <!-- Custom styles for this template -->
+    <link href="headers.css" rel="stylesheet">
 </head>
 <body>
 
