5799c5b1d5067a491918b438d479d19130024bf8
Pablo de Dios <4855797@gmail.com>
2024-09-27 10:23:34 -0600

    Agregar imagenes v75

--- contact.php
+++ contact.php
@@ -12,7 +12,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/css/bootstrap.min.css" rel="stylesheet">
-
+    
 <!------ Include the above in your HEAD tag ---------->
 </head>
 <body>
@@ -34,40 +34,38 @@
         </div>
       </div>
   </header>
-  <nav class="navbar navbar-expand-lg navbar-light bg-info">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+  <nav class="navbar navbar-expand-lg navbar-dark mx-background-top-linear">
+    <div class="container">
+      <a class="navbar-brand" rel="nofollow" target="_blank" href="https://mobilitysolutionscorp.com"> Mobility Solutions Corporation</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Next Steps
+      <ul class="navbar-nav ml-auto">
+
+        <li class="nav-item">
+        <a class="nav-link" href="https://mobilitysolutionscorp.com">Inicio
+            <span class="sr-only">(current)</span>
         </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Contact us</a>
-          <a class="dropdown-item" href="#">Subscribe to the blog</a>
-          <a class="dropdown-item" href="#">Sign up for our newsletter</a>
-        </div>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
+        </li>
+
+        <li class="nav-item">
+        <a class="nav-link" href="https://mobilitysolutionscorp.com/catalogo.php">Catálogo</a>
+        </li>
+
+        <li class="nav-item">
+        <a class="nav-link" href="https://mobilitysolutionscorp.com/about_us.php">Nosotros</a>
+        </li>
+
+        <li class="nav-item active">
+        <a class="nav-link" href="https://mobilitysolutionscorp.com/contact.php">Contacto</a>
+        </li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
 </div>
 
 
