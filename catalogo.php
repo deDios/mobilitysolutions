390c21df131dab47d260d94e638b577a51997f5a
Pablo de Dios <4855797@gmail.com>
2024-09-27 18:47:49 -0600

    Agregar imagenes v131

--- catalogo.php
+++ catalogo.php
@@ -75,14 +75,14 @@
         <h1>Catálogo</h1>
     </div>
 
-    <form action="#">
-      <div class="input-group">
-        <input type="text" class="form-control" placeholder="Search" name="search">
-        <div class="input-group-btn">
-          <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-        </div>
-      </div>
-    </form>
+<div class="demo">
+  <form class="form-search">
+    <div class="input-group">
+      <input class="form-control form-text" maxlength="128" placeholder="Buscar" size="15" type="text" />
+      <span class="input-group-btn"><button class="btn btn-primary"><i class="fa fa-search fa-lg">&nbsp;</i></button></span>
+    </div>
+  </form>
+</div>
 
     <div class="container-items">
         <div class="menu_item">
