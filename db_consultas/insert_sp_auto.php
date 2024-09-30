f36d593987b2fbe5d6c640fa65744b10cbc4f4a5
Pablo de Dios <4855797@gmail.com>
2024-09-30 10:41:53 -0600

    Agregar imagenes v174

--- db_consultas/insert_sp_auto.php
+++ db_consultas/insert_sp_auto.php
@@ -1,12 +1,13 @@
 <?php
 $marca = $_POST['InputMarca'];
+$auto_1 = $_POST['InputNombre'];
 $auto = $_POST['InputAuto'];
 
 $auto_valor = '"' . $auto . '"';
 
 $inc = include "../db/Conexion.php"; 
 $query = 'insert into tmx_marca_auto (marca, auto, updated_at) values 
-('.$auto.','.$auto_valor.',NULL);';
+('.$marca.','.$auto_valor.',NULL);';
         
 $result = mysqli_query($con,$query); 
 if ($result){ 
