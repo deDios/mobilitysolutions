4dcf2e717b36825b7f326f3bec6110ef67c20bf2
Pablo de Dios <4855797@gmail.com>
2024-10-01 10:52:51 -0600

    Agregar imagenes v208

--- detalles.php
+++ detalles.php
@@ -4,38 +4,34 @@ $cod=$_REQUEST['cod'];
 
 $inc = include "db/Conexion.php"; 
 $query = 'select 
-                auto.id,
-                m_auto.auto as nombre, 
-                modelo.nombre as modelo, 
-                marca.nombre as marca, 
-                auto.mensualidad, 
-                auto.costo, 
-                sucursal.nombre as sucursal, 
-                auto.img1, 
-                auto.img2, 
-                auto.img3, 
-                auto.img4, 
-                auto.img5, 
-                auto.img6, 
-                auto.color, 
-                auto.transmision, 
-                auto.interior, 
-                auto.kilometraje, 
-                auto.combustible, 
-                auto.cilindros, 
-                auto.eje, 
-                auto.estatus, 
-                auto.pasajeros, 
-                auto.propietarios,
-                auto.created_at, 
-                auto.updated_at 
-            FROM mobility_solutions.tmx_auto as auto
-            left join mobility_solutions.tmx_sucursal as sucursal on auto.sucursal = sucursal.id 
-            left join mobility_solutions.tmx_estatus as estatus on auto.estatus = estatus.id
-            left join mobility_solutions.tmx_modelo as modelo on auto.modelo = modelo.id 
-            left join mobility_solutions.tmx_marca as marca on auto.marca = marca.id
-            left join mobility_solutions.tmx_marca_auto as m_auto on auto.nombre = m_auto.id
-            where auto.id = '. $cod .';';
+                                    id, 
+                                    nombre, 
+                                    modelo, 
+                                    marca, 
+                                    mensualidad, 
+                                    costo, 
+                                    sucursal, 
+                                    img1, 
+                                    img2, 
+                                    img3, 
+                                    img4, 
+                                    img5, 
+                                    img6, 
+                                    color, 
+                                    transmision, 
+                                    interior, 
+                                    kilometraje, 
+                                    combustible, 
+                                    cilindros, 
+                                    eje, 
+                                    estatus, 
+                                    pasajeros, 
+                                    propietarios, 
+                                    created_at, 
+                                    updated_at, 
+                                    search_key
+                                  from mobility_solutions.v_catalogo_active 
+            where id = '. $cod .';';
 
         $result = mysqli_query($con,$query); 
         if ($result){ 
