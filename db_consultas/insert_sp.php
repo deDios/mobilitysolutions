f191f98861a3b1a285254a136ef2d16f8746cb64
Pablo de Dios <4855797@gmail.com>
2024-10-02 12:52:36 -0600

    Agregar imagenes v232

--- db_consultas/insert_sp.php
+++ db_consultas/insert_sp.php
@@ -82,14 +82,24 @@ $carpeta_id = '/home/site/wwwroot/Imagenes/Catalogo/Auto '.$con->insert_id.'';
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Resultado de carga</title>
+    <link rel="stylesheet" href="CSS/detalles.css">
+
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
 <body>
     
 <br>
-<a href="editar_cat.php"  class="btn btn-primary mt-5">Regresar a Inicio</a>
+<a href="../editar_cat.php"  class="btn btn-primary mt-5">Regresar a Inicio</a>
 
 
 </body>
