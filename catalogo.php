8b39e95945af74e2115459f9dacdcb326781a895
Pablo de Dios <4855797@gmail.com>
2024-10-05 01:07:44 -0600

    Agregar imagenes v243

--- catalogo.php
+++ catalogo.php
@@ -85,6 +85,15 @@
               </form>
             </div>
 
+
+            <div class="card mb-3" style="max-width: 18rem;">
+              <div class="card-header text-white bg-primary">Header</div>
+              <div class="card-body">
+                <h5 class="card-title">Primary card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+
           </div>
         </div>
 
