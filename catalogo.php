4a0ceb6b06525b3f9c93ea2f9ce381499dbc7d55
Pablo de Dios <4855797@gmail.com>
2024-10-08 12:13:47 -0600

    Aviso de privacidad v82

--- catalogo.php
+++ catalogo.php
@@ -98,7 +98,59 @@
                   Reseñas
               </div>
               <div class="card-body">
-                
+
+<!--------------------------------------- Inicia carrusel de reseñas ----------------------------------------------->  
+                <div class="carousel-reviews broun-block">
+                  <div class="container">
+                      <div class="row">
+                          <div id="carousel-reviews" class="carousel slide" data-ride="carousel">                        
+                            <div class="carousel-inner">
+                 
+                              <div class="item active">    
+                                <div class="col-md-4 col-sm-6">
+                                    <div class="block-text rel zmin">
+                                      <a title="" href="#">Hercules</a>
+                                      <div class="mark">My rating: 
+                                        <span class="rating-input">
+                                          <span data-value="0" class="glyphicon glyphicon-star"></span>
+                                          <span data-value="1" class="glyphicon glyphicon-star"></span>
+                                          <span data-value="2" class="glyphicon glyphicon-star"></span>
+                                          <span data-value="3" class="glyphicon glyphicon-star"></span>
+                                          <span data-value="4" class="glyphicon glyphicon-star"></span>
+                                          <span data-value="5" class="glyphicon glyphicon-star"></span>  
+                                        </span>
+                                      </div>
+                                        <p>Never before has there been a good film portrayal of ancient Greece's favourite myth. So why would Hollywood start now? This latest attempt at bringing the son of Zeus to the big screen is brought to us by X-Men: The last Stand director Brett Ratner. If the name of the director wasn't enough to dissuade ...</p>
+                                        <ins class="ab zmin sprite sprite-i-triangle block"></ins>
+                                    </div>
+                                </div>
+                              </div>
+
+                              <div class="item">
+                                  <div class="col-md-4 col-sm-6">
+                                          <div class="block-text rel zmin">
+                                          <a title="" href="#">Hercules</a>
+                                        <div class="mark">My rating: <span class="rating-input"><span data-value="0" class="glyphicon glyphicon-star"></span><span data-value="1" class="glyphicon glyphicon-star"></span><span data-value="2" class="glyphicon glyphicon-star"></span><span data-value="3" class="glyphicon glyphicon-star"></span><span data-value="4" class="glyphicon glyphicon-star-empty"></span><span data-value="5" class="glyphicon glyphicon-star-empty"></span>  </span></div>
+                                          <p>Never before has there been a good film portrayal of ancient Greece's favourite myth. So why would Hollywood start now? This latest attempt at bringing the son of Zeus to the big screen is brought to us by X-Men: The last Stand director Brett Ratner. If the name of the director wasn't enough to dissuade ...</p>
+                                        <ins class="ab zmin sprite sprite-i-triangle block"></ins>
+                                        </div>
+                                  </div>
+                              </div>
+                                                        
+                            </div>
+                              
+                            <a class="left carousel-control" href="#carousel-reviews" role="button" data-slide="prev">
+                                  <span class="glyphicon glyphicon-chevron-left"></span>
+                            </a>
+                            <a class="right carousel-control" href="#carousel-reviews" role="button" data-slide="next">
+                                  <span class="glyphicon glyphicon-chevron-right"></span>
+                            </a>
+
+                          </div>
+                      </div>
+                  </div>
+                </div>
+<!--------------------------------------- Termina carrusel de reseñas ----------------------------------------------->  
               </div>
           </div>
 
