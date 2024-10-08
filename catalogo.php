f49040de1d5f03f726060eb0635ccdf473192840
Pablo de Dios <4855797@gmail.com>
2024-10-07 20:56:02 -0600

    Aviso de privacidad v22

--- catalogo.php
+++ catalogo.php
@@ -89,16 +89,16 @@
           </div>
 
           <div class="anuncios">
-          <p><small>Referencias</small></p>
-            <hr class="mt-2 mb-3"/>
-            <div>
+            <p><small>Referencias</small></p>
+              <hr class="mt-2 mb-3"/>
+              <div>
 
-            </div>
-          <p><small>Publicidad</small></p>
-            <hr class="mt-2 mb-3"/>
-            <div>
-              
-            </div>
+              </div>
+            <p><small>Publicidad</small></p>
+              <hr class="mt-2 mb-3"/>
+              <div>
+                
+              </div>
           </div>
         </div>
 
