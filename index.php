e3101d4b1df5da55f458e0efbd5d54759ffd1d03
Pablo de Dios <4855797@gmail.com>
2024-10-09 13:20:02 -0600

    Aviso de privacidad v96

--- index.php
+++ index.php
@@ -37,7 +37,11 @@
   <nav class="navbar navbar-expand-lg navbar-dark mx-background-top-linear">
     <div class="container">
       <a class="navbar-brand" rel="nofollow" target="_blank" href="https://mobilitysolutionscorp.com"> Mobility Solutions Corporation</a>
-      
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+
         <ul class="navbar-nav ml-auto">
 
           <li class="nav-item active">
@@ -60,7 +64,7 @@
 
         </ul>
 
-     
+      </div>
     </div>
   </nav>
 </div>
