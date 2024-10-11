cf9189145089703d5204d6ea00d16da0a93fb6a7
Pablo de Dios <4855797@gmail.com>
2024-10-11 02:12:06 -0600

    Carrusel infinito v27

--- index.php
+++ index.php
@@ -209,8 +209,7 @@
                   </div>
               </div>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
+              <a href="#" class="card-link">Read more</a>
             </div>
           </div>
         </div>
