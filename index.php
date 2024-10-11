0b665a11c7a70eba01c525642230e005e2315c57
Pablo de Dios <4855797@gmail.com>
2024-10-11 02:31:10 -0600

    Carrusel infinito v28

--- index.php
+++ index.php
@@ -216,7 +216,6 @@
                 <label for="radio5">★</label>
               </p>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="card-link">Read more</a>
             </div>
           </div>
         </div>
