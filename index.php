f4abd001c1981341a12a8dc688f6c9b24eabc06a
Pablo de Dios <4855797@gmail.com>
2024-10-11 01:19:53 -0600

    Carrusel infinito v8

--- index.php
+++ index.php
@@ -196,9 +196,20 @@
     <div class="slide-track">
       
         <div class="slides">
-          <img src="logo_google.jpg" alt="logo 1">
-           A
+
+          <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <img src="logo_google.jpg" alt="logo 1">
+              <h5 class="card-title">Card title</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="card-link">Card link</a>
+              <a href="#" class="card-link">Another link</a>
+            </div>
+          </div>
+
         </div>
+
         <div class="slides">
           <img src="logo_google.jpg" alt="logo 1">
           B
