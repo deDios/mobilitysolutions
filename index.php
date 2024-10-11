6a67bf50dfa85b4d4867b5b87b73d0cd4ad0ef06
Pablo de Dios <4855797@gmail.com>
2024-10-11 01:42:04 -0600

    Carrusel infinito v15

--- index.php
+++ index.php
@@ -196,7 +196,7 @@
     <div class="slide-track">
       
         <div class="cartas">
-          <div class="card" style="width: 18rem;">
+          <div class="card l-2" style="width: 18rem;">
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
               <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
@@ -208,7 +208,7 @@
         </div>
 
         <div class="cartas">
-          <div class="card" style="width: 18rem;">
+          <div class="card l-2" style="width: 18rem;">
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
               <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
