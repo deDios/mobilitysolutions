67c319d8fe027dbf956b1fdca031dc5e97f0d52c
Pablo de Dios <4855797@gmail.com>
2024-10-15 17:41:01 -0600

    Update vehiculo v22

--- update_reg.php
+++ update_reg.php
@@ -188,7 +188,7 @@ $inc = include "db/Conexion.php";
                     <!-- Input Color ------------------------------------------------------------------->
                     <div class="col-2 mt-5">
                         <label for="InputColor" class="form-label">Color</label>
-                        <select id="InputColor" class="form-select" aria-label="Default select example" name="InputColor" value="'Verde'">
+                        <select id="InputColor" class="form-select" aria-label="Default select example" name="InputColor" value="<?php echo $color_get;?>">
                             <option value="Negro">Negro</option>  
                             <option value="Rojo">Rojo</option>  
                             <option value="Azul">Azul</option>  
