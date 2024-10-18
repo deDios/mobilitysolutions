901c19b6ac14a03517391bd77ba0ef7a393ad135
Pablo de Dios <4855797@gmail.com>
2024-10-18 13:08:27 -0600

    Update vehiculo v36

--- Test/catalogo_test.php
+++ Test/catalogo_test.php
@@ -4,8 +4,8 @@
  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Catálogo</title>
-    <link rel="stylesheet" href="Test/CSS/catalogo_test.css">
+    <title>Test de Catálogo</title>
+    <link rel="stylesheet" href="CSS/catalogo_test.css">
 
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
@@ -98,7 +98,7 @@
 
         <div class="lista_item">
             <?php 
-                $inc = include "db/Conexion.php";    
+                $inc = include "../db/Conexion.php";    
                 if(isset($_GET['enviar'])) {
                   $busqueda = $_GET['buscar'];
                   $busqueda = trim($busqueda);
