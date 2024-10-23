65ffbaf248d3c80c15d62b39929730ea2edb4011
Pablo de Dios <4855797@gmail.com>
2024-10-22 19:55:21 -0600

    Update vehiculo v121

--- Views/login.php
+++ Views/login.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="en-MX">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="CSS/loging.css">
+    <link rel="stylesheet" href="CSS/login.css">
     <title>Login Validate</title>
 </head>
 <body>
