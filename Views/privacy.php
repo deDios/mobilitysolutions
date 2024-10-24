a143858c93e9b6b6edd32da0a4b49be80f4947ea
Pablo de Dios <4855797@gmail.com>
2024-10-24 11:39:36 -0600

    Update vehiculo v181

--- Views/privacy.php
+++ Views/privacy.php
@@ -78,7 +78,7 @@ $pdf_file = "/DOCS/AP.pdf";
     <div class="col-lg-8 col-md-8 mt-5 mx-auto">
         <h1 class="fw-light mt-5 text-center">Aviso de Privacidad</h1>
         
-        <h4> I.- FINALIDAD DEL TRATAMIENTO Y DATOS PERSONALES RECABADOS POR EL RESPONSABLE. </h4>
+        <h6> I.- FINALIDAD DEL TRATAMIENTO Y DATOS PERSONALES RECABADOS POR EL RESPONSABLE. </h6>
         <p class="lead lh-sm text-muted text-justify">
             MSC únicamente solicita los datos personales que sean necesarios para llevar a cabo las finalidades descritas en el
             presente Aviso de Privacidad, el cual se hace del conocimiento de los clientes o prospectos antes de que éstos los
