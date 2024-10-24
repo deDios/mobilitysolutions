bae0e3d558d05406b348d617ecb437aa7fa03d16
Pablo de Dios <4855797@gmail.com>
2024-10-24 11:03:02 -0600

    Update vehiculo v171

--- Views/privacy.php
+++ Views/privacy.php
@@ -75,13 +75,189 @@ $pdf_file = "/DOCS/AP.pdf";
 </div>
     
 
-        <main>
-            <section>
-                <div class="embed-responsive embed-responsive-16by9 mt-5">
-                    <iframe class="embed-responsive-item" src="<?php echo $pdf_file; ?>" allowfullscreen></iframe>
-                </div>
-            </section>
-        </main>
+    <div class="col-lg-6 col-md-8">
+        <h1 class="fw-light mx-auto">Aviso de Privacidad</h1>
+        <p class="lead text-muted">
+        <h2> I.- FINALIDAD DEL TRATAMIENTO Y DATOS PERSONALES RECABADOS POR EL RESPONSABLE. </h2>
+            MSC únicamente solicita los datos personales que sean necesarios para llevar a cabo las finalidades descritas en el
+            presente Aviso de Privacidad, el cual se hace del conocimiento de los clientes o prospectos antes de que éstos los
+            proporcionan. Los datos personales que MSC solicita u obtiene de sus clientes o prospectos son los siguientes:
+            • Datos Personales de Identificación
+            • Datos Personales de Contacto
+            • Datos Personales Laborales
+            • Datos Personales Financieros
+            • Datos Personales Sensibles, consistentes en datos biométricos
+            • Datos electrónicos, y en particular datos de geolocalización
+            Asimismo, se le informa que MSC puede obtener los datos personales mediante cualquiera de los siguientes mecanismos:
+            A. Obtenerlos directamente del cliente o prospecto, o bien, a través de terceros proveedores de servicios que actúan
+            a nombre de MSC.
+            B. Por medio de la red de agencias de vehículos autorizadas de todas las marcas (en lo sucesivo el "Proveedor de
+            los Vehículos"). Toda vez que, MSC trabaja con los proveedores de los vehículos para recabar información de los
+            clientes o prospectos, es importante señalar son personas morales distintas e independientes a MSC. Por lo
+            anterior, MSC no es responsable del tratamiento que dichos proveedores de los vehículos den a los datos
+            personales que recaban para sus propias finalidades, diferentes las informadas en el presente Aviso de Privacidad.
+            C. Por medio de los fabricantes de los vehículos de todas las marcas (en lo sucesivo el "Fabricante de los
+            Vehículos”). Toda vez que, MSC trabaja con los fabricantes de los vehículos para recabar información de los
+            clientes o prospectos, es importante señalar son personas morales distintas e independientes a MSC. Por lo
+            anterior, MSC no es responsable del tratamiento que dichos fabricantes de los vehículos den a los datos personales
+            que recaban para sus propias finalidades, diferentes las informadas en el presente Aviso de Privacidad.
+            Los clientes o prospectos pueden acceder a los sitios web de MSC sin necesidad de proporcionar datos personales. Sin
+            embargo, para poder utilizar ciertas funciones de los sitios web de MSC como cotizaciones de productos y/o servicios, será
+            necesario que los interesados se registren en el sitio web o proporcionen sus datos personales. Como parte de este registro
+            voluntario, MSC podrá solicitar los siguientes datos personales: datos personales de identificación, datos personales de
+            contacto y datos personales patrimoniales, los cuales serán necesarios para el funcionamiento de esos servicios y/o
+            promociones. En caso de que los clientes o prospectos compartan datos personales en áreas públicas de los sitios web de
+            MSC y/o redes sociales, estos datos personales podrán ser vistos y utilizados por otros, sin responsabilidad alguna para
+            MSC.
+            MSC trata los datos personales de clientes para las siguientes finalidades primarias:
+            i. Acreditar y verificar la identidad de nuestros clientes o prospectos, de acuerdo con los procedimientos y políticas
+            de MSC.
+            ii. Autenticarlo a través de la información que proporcione para el cumplimiento con las disposiciones legales
+            aplicables.
+            iii. En su caso, verificar los datos de su identificación oficial, incluidos aquellos datos considerados como biométricos,
+            a través de los mecanismos establecidos con el Instituto Nacional Electoral.
+            iv. Llevar a cabo evaluaciones de la información crediticia que la o las sociedades de información crediticia nos
+            proporcionen, previa autorización de su parte.
+            v. En su caso, celebrar el correspondiente contrato de apertura de crédito simple con garantía prendaria para la
+            adquisición de vehículos nuevos y/o seminuevos, así como los accesorios que deriven del mismo;
+            vi. En su caso, celebrar el correspondiente contrato de vehículos nuevos y/o seminuevos.
+            vii. Para la cotización y/o contratación de productos y/o servicios ofertados por terceros relacionados con nuestros
+            créditos.
+            viii. Para el procesamiento de las solicitudes de crédito.
+            ix. Para realizar gestiones de cobranza, buscando la recuperación de cartera vencida, así como para realizar acciones
+            preventivas o reactivas con esa finalidad, incluyendo la ubicación geográfica del vehículo que funge como garantía
+            prendaría en términos del contrato (“Contrato”).
+            x. Para dar cumplimiento a obligaciones contraídas con nuestros clientes, así como a requerimientos de autoridades
+            judiciales y/o administrativas.
+            xi. Mantener actualizados sus datos personales en los sistemas operados con la red de proveedores de los vehículos
+            de todas las marcas.
+            xii. Archivo y registro legal de contratos, pagos, requerimientos de pago y cualquier otra información relacionada con
+            la relación jurídica existente entre usted y MSC, incluyendo el uso de esa información
+            xiii. Las que resulten compatibles o análogas con las demás finalidades para las que MSC hubiese recabado sus datos
+            personales en primer lugar.
+            De manera adicional, MSC tratará los datos personales de clientes para las siguientes finalidades secundarias, que
+            permiten y facilitan a MSC brindarle una mejor atención:
+            i. Comunicarle por medios físico y/o electrónicos, actividades promocionales y de prospección de MSC.
+            ii. Ofrecerle productos e información de las marcas, así como de nuestros socios de negocios.
+            iii. Realizar análisis estadísticos y de mercado.
+            En caso de que cualquiera de los clientes no desee que sus datos personales sean tratados para los fines adicionales
+            descritos en este párrafo, puede presentar desde este momento una solicitud manifestando lo anterior, conforme al proceso
+            que para ejercitar su derecho de acceso, rectificación, cancelación y oposición (ARCO), mismo que se define más adelante.
+            La negativa para el tratamiento de sus datos personales para estas finalidades no serán un motivo para que se le niegue
+            los servicios y productos que solicita o contrata con MSC.
+            MSC trata los datos personales de los prospectos para las siguientes finalidades primarias:
+            i. Bajo demanda del titular, atender las solicitudes sobre el posible otorgamiento de algún producto financiero y/o
+            arrendamiento de MSC.
+            ii. Comunicar por cualquier medio autorizado las cotizaciones de productos y servicios de MSC.
+            De manera adicional, MSC tratará los datos personales de prospectos para las siguientes finalidades secundarias, que
+            permiten y facilitan a MSC brindarle una mejor atención:
+            i. Comunicarle por medios físicos y/o electrónicos, actividades promocionales y de prospección de MSC.
+            ii. Ofrecerle productos e información de las marcas, así como de nuestros socios de negocios.
+            iii. Realizar análisis estadísticos y de mercado.
+            En caso de que cualquier prospecto, no desee que sus datos personales sean tratados para estos fines adicionales, puede
+            presentar desde este momento una solicitud manifestando lo anterior, conforme al proceso que para ejercitar su derecho
+            de acceso, rectificación, cancelación y oposición (ARCO), mismo que se define más adelante. La negativa para el uso de
+            sus datos personales para estas finalidades no serán un motivo para que se le niegue los servicios y productos que solicita
+            o contrata con MSC.
+            <h2>II.- TRANSFERENCIA Y REMISIÓN DE DATOS PERSONALES</h2>
+            MSC puede transferir y/o remitir sus datos personales a terceros con los que MSC tenga una relación jurídica tales como
+            proveedores de servicios, aseguradoras, procesadores de tarjetas de crédito, proveedores de administración de bases de
+            datos, auditores, despachos de cobranza, consultores, Proveedor de vehículos donde el ACREDITADO haya adquirido el
+            vehículo objeto de la relación jurídica con el responsable, proveedor y/o el fabricante del/los vehículo(s), a efecto de llevar
+            a cabo las finalidades previstas en este Aviso de Privacidad o hacer efectivo el cumplimiento de las obligaciones que
+            emanen de la relación jurídica entre usted y MSC.
+            Para aquellas transferencias que requieren de su consentimiento, a través del presente aviso de privacidad usted acepta
+            y reconoce que nos otorga su consentimiento, en caso de que se oponga podrá manifestar su negativa a través del
+            correo atcliente@mobilitysolutionscorp.com
+            Asimismo, se le informa que conforme a lo previsto en el artículo 37 fracción III, IV y VII, de la Ley Federal de Protección
+            de Datos Personales en Posesión de los Particulares, MSC podrá transferir sus datos personales a los siguientes terceros:
+            I. La red de agencias de vehículos autorizadas de todas las marcas ("Proveedor de los vehículos y/o distribuidor de
+            vehículos indistintamente”).
+            II. Empresas de Seguros.
+            Los destinatarios de datos personales trataran esta información conforme a las finalidades descritas en el capítulo que
+            antecede “FINALIDADES DEL TRATAMIENTO DE LOS DATOS PERSONALES”.
+            <h2>III.- SEGURIDAD DE LOS DATOS PERSONALES: </h2>
+            MSC está comprometido a salvaguardar la seguridad de los datos personales de sus clientes y prospectos, por lo que, ha
+            implementado diversos mecanismos de seguridad físicos, administrativos y tecnológicos para protegerlos contra cualquier
+            daño, pérdida, alteración, destrucción o el tratamiento no autorizado. En el mismo sentido, MSC tiene políticas y
+            procedimientos para la seguridad de la información y sus terceros proveedores de servicio están comprometidos a
+            respetarlas.
+            <h2>IV.- EJERCICIO DE DERECHOS ARCO:</h2>
+            Todos los clientes o prospectos de MSC tienen derecho a:
+            a. Conocer qué datos personales tiene MSC de ellos, para qué los utiliza y las condiciones del tratamiento (derecho
+            de acceso);
+            b. Solicitar la corrección de su información personal, en caso de que esté desactualizada, sea inexacta o incompleta
+            (derecho de rectificación);
+            c. Solicitar que se elimine de los registros o bases de datos de MSC sus datos personales, cuando considere que la
+            misma no está siendo utilizada conforme a los principios, deberes y obligaciones previstas en la legislación
+            aplicable (derecho de cancelación);
+            d. Oponerse al uso de sus datos personales para fines específicos (derecho de oposición).
+            Todos estos derechos de acceso, rectificación, cancelación y oposición se conocen como derechos ARCO; mismos que
+            se pueden ejercer conforme al siguiente procedimiento:
+            1. Conforme lo previsto en el artículo 29 de la LFPDPPP, para ejercer algún derecho ARCO deberá elaborar una
+            solicitud, dirigida a MSC y enviarla vía correo electrónico a atcliente@mobilitysolutionscorp.com. Dicha solicitud
+            deberá contener todos los siguientes puntos:
+            o Nombre completo del titular del derecho reclamado y domicilio u otro medio para comunicarle la r
+            o Respuesta a su solicitud.
+            o Documentos que acrediten la identidad o, en su caso, la representación legal del titular del derecho
+            reclamado.
+            o Descripción clara y precisa de los datos personales respecto de los que se busca ejercer alguno de los
+            derechos que marca la LFPDPPP.
+            o Cualquier otro elemento o documento que facilite la localización de los datos personales.
+            2. Dentro de los cinco días hábiles siguientes a la recepción de su solicitud, MSC podrá solicitarle que aporte
+            información adicional en caso de que su solicitud este incompleta, errónea o no contenga todos los puntos que
+            refiere el artículo 29 de LFPDPPP.
+            3. En el supuesto del punto anterior, el cliente o prospecto tendrá un lapso de diez días hábiles contados a partir del
+            día siguiente en que lo haya recibido correo por parte de MSC, para atender al requerimiento de información
+            adicional.
+            4. Posteriormente, conforme a lo dispuesto en los artículos del 28 al 35 de la LFPDPPP, MSC le comunicará la
+            determinación adoptada en un plazo máximo de veinte días contados desde la recepción de su solicitud si resulta
+            procedente, MSC tendrá un lapso de quince días para adoptar dicha determinación. MSC puede ampliar los plazos
+            señalados en el presente punto por sola vez por un periodo igual, siempre y cuando así lo justifiquen las
+            circunstancias del caso.
+            Los clientes y prospectos de MSC pueden, en todo momento, revocar el consentimiento otorgado a MSC para el tratamiento
+            de sus datos personales, conforme al procedimiento antes descrito. Sin embargo, es importante que tenga en cuenta que
+            no en todos los casos MSC podrá atender su solicitud o concluir el uso de forma inmediata, ya que es posible que por
+            alguna obligación legal requiera seguir tratando sus datos personales.
+            <h2>V.- USO DE COOKIES:</h2>
+            MSC le informa que durante la navegación en este sitio web se utilizan cookies. Las cookies son asientos de memoria que
+            pueden quedar almacenadas dentro de la computadora del usuario y de la cual MSC obtiene automáticamente la siguiente
+            información:
+            • La fecha y hora de acceso al sitio web de MSC.
+            • Tiempo de navegación en el sitio web de MSC.
+            • El número de visitas diarias en cada sección del sitio web de MSC.
+            • La fecha y hora de la última vez que el usuario visitó el sitio web de MSC.
+            • El diseño de contenidos que el usuario escogió en su primera visita al sitio web de MSC.
+            • Páginas de Internet accedidas previo a la visita al sitio web de MSC.
+            El uso de las cookies le permite a MSC adaptar y mejorar sus servicios a los intereses del usuario, para brindarle un mejor
+            servicio y experiencia de usuario al navegar en este sitio web. Sin embargo, el usuario tiene la opción de impedir la
+            generación de cookies, mediante la inhabilitación de dicha función desde su programa navegador. Para tal efecto, puede
+            consultar el proceso de eliminación de cookies en los siguientes navegadores más comunes:
+            • Chrome: Configuración -> Mostrar opciones avanzadas -> Privacidad -> Configuración de contenido. Para más
+            información, puede consultar el soporte de Google o la Ayuda del navegador.
+            • Firefox: Herramientas -> Opciones -> Privacidad -> Historial -> Configuración Personalizada. Para más
+            información, puede consultar el soporte de Mozilla o la Ayuda del navegador.
+            • Internet Explorer: Herramientas -> Opciones de Internet -> Privacidad -> Configuración. Para más información,
+            puede consultar el soporte de Microsoft o la Ayuda del navegador.
+            • Safari: Preferencias -> Seguridad. Para más información, puede consultar el soporte de Apple o la Ayuda del
+            navegado.
+            <h2>VI.- CAMBIOS AL AVISO DE PRIVACIDAD:</h2>
+            MSC se reserva el derecho a modificar el presente Aviso de Privacidad, para adaptarlo a novedades legislativas o
+            jurisprudenciales, cambios o actualizaciones en las propias del tratamiento de los datos personales. En dichos supuestos,
+            MSC se compromete a mantenerlo informado sobre dichos cambios, anunciando la versión más reciente del presente
+            Aviso de Privacidad, en el sitio web de MSC.
+            <h2>VII.- DEPARTAMENTO DE PROTECCIÓN DE DATOS PERSONALES:</h2>
+            MSC le informa que cuenta con un órgano interno enfocado a la protección de datos personales y garantizar el debido
+            tratamiento de los datos personales. Para tal efecto, dicho órgano interno verifica la implementación de las medidas de
+            seguridad físicas, administrativas y tecnológicas, que garanticen la seguridad de sus datos personales. Asimismo, dicho
+            órgano interno de MSC designó un departamento de protección de datos personales, que tiene a su cargo la recepción,
+            registro y atención de sus solicitudes de derechos ARCO, conforme al proceso antes descrito. Por lo que, para cualquier
+            solicitud, aclaración o comentario relacionado con el presente Aviso de Privacidad sírvase contactar a dicho departamento
+            de NRFM, vía correo electrónico atcliente@mobilitysolutionscorp.com
+            Última Actualización en la Ciudad de México al 17 de MAYO de 2019.
+        </p>
+        
+    </div>
 
     <footer>
         <p>&copy; <?php echo date("Y"); ?> Mobility Solutions Corporation. Todos los derechos reservados.</p>
