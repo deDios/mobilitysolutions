3ab7ee81565e42855f956b7b905902a3bd642991
Pablo de Dios <4855797@gmail.com>
2024-11-06 01:37:29 -0600

    Update vehiculo v219

--- buscar_carro.php
+++ buscar_carro.php
@@ -5,12 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Buscador de Carros</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> <!-- Usamos jQuery para facilitar el AJAX -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.0/jquery.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
-    <style>
+  <style>
         /* Reset de estilos */
         * {
             margin: 0;
