3cd4d1737cbcd6eb50b82017ede39d057d08484f
Pablo de Dios <4855797@gmail.com>
2024-11-06 16:16:02 -0600

    Filtro catalogo v14

--- Test/catalogo_test.php
+++ Test/catalogo_test.php
@@ -88,7 +88,7 @@
 
           <div class="lay_ser"> 
             <h3 class="fw-light">Filtros</h3>
-            <div class="accordion accordion-flush mx-auto py-5" id="accordionFlushExample">
+            <div class="accordion mx-auto py-5" id="accordionFlushExample">
                   <div class="accordion-item">
                       <h2 class="accordion-header">
                           <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
