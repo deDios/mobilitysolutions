90ef2e4c0d912cc6b29956b1a7a4bf033005cf20
Pablo de Dios <4855797@gmail.com>
2024-11-06 18:00:16 -0600

    Filtro catalogo v37

--- Test/catalogo_test.php
+++ Test/catalogo_test.php
@@ -74,7 +74,7 @@
     
         <div class="menu_item">
           
-          <a class="btn btn-light" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+          <a class="btn btn-light py-5" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
             <i class="fa fa-filter"></i>
           </a>
           <div class="collapse py-2" id="collapseExample">
