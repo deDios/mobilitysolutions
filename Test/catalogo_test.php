5e71e5d0075d85c941251134c211b27c8f72bc35
Pablo de Dios <4855797@gmail.com>
2024-11-07 18:08:11 -0600

    Filtro catalogo v95

--- Test/catalogo_test.php
+++ Test/catalogo_test.php
@@ -134,24 +134,6 @@
                                 <option value="0">Selecciona un Modelo</option> 
                             </select>
                         </div>
-                        <div class="pt-1">
-                          <select id="InputAnio" class="form-select" aria-label="Default select example" name="InputAnio">
-                              <option value="Todos">Selecciona año</option> 
-                              <option value="2013">2013</option> 
-                              <option value="2014">2014</option> 
-                              <option value="2015">2015</option> 
-                              <option value="2016">2016</option> 
-                              <option value="2017">2017</option> 
-                              <option value="2018">2018</option> 
-                              <option value="2019">2019</option> 
-                              <option value="2020">2020</option> 
-                              <option value="2021">2021</option> 
-                              <option value="2022">2022</option> 
-                              <option value="2023">2023</option> 
-                              <option value="2024">2024</option> 
-                              <option value="2025">2025</option> 
-                          </select>
-                        </div>
 
                             <script >
                               $(document).ready(function(){
@@ -198,6 +180,27 @@
                     </h2>
                     <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
                       <div class="accordion-body">
+
+                        <div class="pt-1">
+                          <select id="InputAnio" class="form-select" aria-label="Default select example" name="InputAnio">
+                              <option value="Todos">Selecciona año</option> 
+                              <option value="2013">2013</option> 
+                              <option value="2014">2014</option> 
+                              <option value="2015">2015</option> 
+                              <option value="2016">2016</option> 
+                              <option value="2017">2017</option> 
+                              <option value="2018">2018</option> 
+                              <option value="2019">2019</option> 
+                              <option value="2020">2020</option> 
+                              <option value="2021">2021</option> 
+                              <option value="2022">2022</option> 
+                              <option value="2023">2023</option> 
+                              <option value="2024">2024</option> 
+                              <option value="2025">2025</option> 
+                          </select>
+                        </div>
+
+
                         <div class="pt-1">
                           <select id="InputColor" class="form-select" aria-label="Default select example" name="InputColor">
                               <option value="Todos">Selecciona un color</option> 
