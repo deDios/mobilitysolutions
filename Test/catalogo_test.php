e8a9e9155a287b35dd2ce5861d161e571eb0f95b
Pablo de Dios <4855797@gmail.com>
2024-11-08 09:57:02 -0600

    Filtro catalogo v169

--- Test/catalogo_test.php
+++ Test/catalogo_test.php
@@ -493,9 +493,8 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
-<hr class="mt-2 mb-3"/>
 
-<footer>  
+<footer class="mt-5">  
   <div class="container">    
     <div class="row">      
       <div class="col-lg-3">        
