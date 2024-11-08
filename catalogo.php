1c874c35846ca46fcdb4c29e78ef0fa82878be9e
Pablo de Dios <4855797@gmail.com>
2024-11-08 11:45:24 -0600

    Pie de pagina v7

--- catalogo.php
+++ catalogo.php
@@ -504,19 +504,47 @@
         }
     </script>
 
-  <footer class="text-muted py-5">
-    <div class="container">
-      <p class="float-end mb-1">
-        <a href="#">Regresa al inicio</a>
-      </p>
-      <p class="mb-1">DERECHOS DE AUTOR &COPY; 2014 - 2024 MOBILITY SOLUTIONTS CORPORATIONS</p>
-      <a title="Descargar Archivo" rel="nofollow" href="/Views/privacy.php" style="color: blue; font-size:18px;"> Consulta nuestro aviso de privacidad 
-      <span class="glyphicon glyphicon-download-alt" aria-hidden="true">
-      </span> 
-      </a>
-      <p class="mb-0">Contactanos: contacto@mobilitysolutionscorp.com</p>
-    </div>
-  </footer>
+<footer class="mt-5">  
+  <div class="container">    
+    <div class="row">      
+      <div class="col-lg-3">        
+        <h6>Conoce más</h6>  
+        <hr class="hr1 mt-2 mb-3"/>      
+        <ul class="text-secondary list-unstyled">
+          <li>
+            <a class="text-secondary" href="https://mobilitysolutionscorp.com/about_us.php">¿Quiénes Somos?</a>
+          </li>
+        </ul>     
+      </div>      
+      <div class="col-lg-3">        
+        <h6>Legales</h6>    
+        <hr class="hr2 mt-2 mb-3"/>    
+        <ul class="text-secondary list-unstyled">
+          <li>
+            <a class="text-secondary" href="/Views/privacy.php">Aviso de privacidad</a>
+          </li>
+        </ul>       
+      </div>      
+      <div class="col-lg-3">        
+        <h6>Ayuda</h6>    
+        <hr class="hr3 mt-2 mb-3"/>    
+        <ul class="text-secondary list-unstyled">
+          <li>
+            <a class="text-secondary" href="https://mobilitysolutionscorp.com/contact.php">Contacto</a>
+          </li>
+          <li>
+            <a class="text-secondary" href="https://mobilitysolutionscorp.com/about_us.php">Preguntas frecuentes</a>
+          </li>
+        </ul>     
+      </div>  
+      <div class="col-lg-3">     
+        <p class="float-end mb-1">
+          <a href="#">Regresa al inicio</a>
+        </p>
+      </div>    
+    </div>  
+  </div>
+</footer>
 
 
 
