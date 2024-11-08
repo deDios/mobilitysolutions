9f5db1b70c6a3d10b9c2c247f2d82a6d2e58ef38
Pablo de Dios <4855797@gmail.com>
2024-11-08 01:24:48 -0600

    Filtro catalogo v134

--- catalogo.php
+++ catalogo.php
@@ -81,9 +81,11 @@
               <div class="input-group input-group-lg">
                     <input class="form-control form-text pl-3" maxlength="128" placeholder="Buscar" type="text" name="buscar" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg"/>
               </div>
-              <a class="btn btn-secondary btn-lg pt-2" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-                <i class="fa fa-filter"></i>
-              </a>
+              <div class="div_icon pt-2">
+                <a class="btn btn-secondary btn-lg" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                  <i class="fa fa-filter"></i>
+                </a>
+              </div>
             
               <div class="collapse py-2" id="collapseExample">
 
