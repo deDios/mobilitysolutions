ae83c80846493351c1dc730bf7df5d94448a19d8
Pablo de Dios <4855797@gmail.com>
2024-11-08 01:59:26 -0600

    Filtro catalogo v145

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
           </li>
 
           <li class="nav-item">
-            <a class="nav-link" href="https://mobilitysolutionscorp.com/catalogo.php">Catálogo</a>
+            <a class="nav-link" href="https://mobilitysolutionscorp.com/catalogo.php?buscar=&InputMarca=Todos&InputAnio=Todos&InputColor=Todos&InputTransmision=Todos&InputInterior=Todos&InputTipo=Todos&InputMensualidad_Mayor=&InputMensualidad_Menor=&enviar=">Catálogo</a>
           </li>
 
          <li class="nav-item">
