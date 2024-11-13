142e4f07282649eda0ceb9e3b68ffb65c915b500
Pablo de Dios <4855797@gmail.com>
2024-11-13 14:47:55 -0600

    Sello en detalle v12

--- Test/detalles.php
+++ Test/detalles.php
@@ -78,6 +78,7 @@ $query = 'select
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
 
 </head>
  <body>
