3dc8e91a8781bb57367066405455f115689a91dc
Pablo de Dios <4855797@gmail.com>
2024-11-13 18:12:21 -0600

    Sello en detalle v19

--- Test/detalles.php
+++ Test/detalles.php
@@ -147,7 +147,7 @@ $query = 'select
                 <img src="../Imagenes/Marcas/logo_<?php echo $marca; ?>.jpg" alt="logo 1">
                 <div class="Descripcion">
                     <div class="texto">  <?php echo $marca . " " . $nombre . " " . $modelo;?> </div>
-                    <div class="texto2"> <?php echo "DESDE $" . number_format($mensualidad) ."/mes | " . "$" . number_format($costo) ;?> </div>
+                    <div class="texto2"> <?php echo "DESDE $" . number_format($mensualidad) ."/mes | " . "$" . number_format($costo);?> <p class="id_t"><?php echo " #".$id;?></p> </div>
                 </div>
             </div>
 <!-------------------------------- Carrusel auto seleccionado -------------------------------------->
