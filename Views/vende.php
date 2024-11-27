908f85004891b12c5f3c272705042657415c61b6
Pablo de Dios <4855797@gmail.com>
2024-11-27 15:06:14 -0600

    Vende tu auto v35

--- Views/vende.php
+++ Views/vende.php
@@ -151,17 +151,17 @@ Envia tu información
 <form role="form">
     <div class="row setup-content" id="step-1">
         <div class="col-xs-12">
-            <div class="row mt-2">
+            <div class="col-md-12">
                 <h3>Datos de contacto</h3>
-                <div class="form-group col-4 mt-2">
+                <div class="form-group">
                     <label class="control-label">Nombre</label>
                     <input  maxlength="100" type="text" required="required" class="form-control" placeholder="Nombre"  />
                 </div>
-                <div class="form-group col-4">
+                <div class="form-group">
                     <label class="control-label">Apellido paterno</label>
                     <input maxlength="100" type="text" required="required" class="form-control" placeholder="Apellido paterno" />
                 </div>
-                <div class="form-group col-4">
+                <div class="form-group">
                     <label class="control-label">Apellido materno</label>
                     <input maxlength="100" type="text" required="required" class="form-control" placeholder="Apellido materno" />
                 </div>
@@ -169,12 +169,12 @@ Envia tu información
                     <label for="inputTel">Telefono de contacto</label>
                     <input type="text" class="form-control" id="inputTel" placeholder="33 31337865">
                 </div>
-                <div class="form-group col-6">
+                <div class="form-group">
                     <label for="inputEmail">Correo electronico</label>
                     <input type="text" class="form-control" id="inputEmail" placeholder="1234@gmail.com">
                 </div>
+                <button class="btn btn-primary nextBtn btn-lg pull-right mt-2" type="button" >Next</button>
             </div>
-            <button class="btn btn-primary nextBtn btn-lg pull-right mt-2" type="button" >Next</button>
         </div>
     </div>
     <div class="row setup-content" id="step-2">
