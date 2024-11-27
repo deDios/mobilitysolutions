604c5985897cd3b881c4f944d2e541859575cd07
Pablo de Dios <4855797@gmail.com>
2024-11-27 09:59:51 -0600

    Vende tu auto v15

--- Views/vende.php
+++ Views/vende.php
@@ -91,15 +91,15 @@
     <div class="stepwizard-row setup-panel">
         <div class="stepwizard-step">
             <a href="#step-1" type="button" class="btn btn-primary btn-circle">1</a>
-            <p>Datos de contacto</p>
+            <p>Contacto</p>
         </div>
         <div class="stepwizard-step">
             <a href="#step-2" type="button" class="btn btn-default btn-circle" disabled="disabled">2</a>
-            <p>Datos de tu auto</p>
+            <p>Auto</p>
         </div>
         <div class="stepwizard-step">
             <a href="#step-3" type="button" class="btn btn-default btn-circle" disabled="disabled">3</a>
-            <p>Enviado</p>
+            <p>Solicitado</p>
         </div>
     </div>
 </div>
