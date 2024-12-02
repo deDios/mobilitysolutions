02f166f1dca56e2e79d55398b146d8a3aaf48b9e
Pablo de Dios <4855797@gmail.com>
2024-12-02 01:31:40 -0600

    Sello en detalle v60

--- Views/vende.php
+++ Views/vende.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vende tu auto</title>
+    <link rel="shortcut icon" href="../Imagenes/movility.ico" />
     <link rel="stylesheet" href="../CSS/vende.css">
 
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
