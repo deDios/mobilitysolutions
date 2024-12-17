6faa0fb7ffde1a84ae0e78195c78fb4460f59baa
Pablo de Dios <4855797@gmail.com>
2024-12-17 10:12:06 -0600

    Pantalla de edición catalogo v48

--- Views/edicion_catalogo.php
+++ Views/edicion_catalogo.php
@@ -89,25 +89,25 @@
 </div>
 <div class="barra_totalizadores">
         <div class="totalizador">
-            <img src="../Imagenes/ver.jpg" alt="logo 1">
+            <div class="fig"><img src="../Imagenes/ver.jpg" alt="logo 1"></div>
             <div class="Total_info">
                 1
             </div>
         </div>
         <div class="totalizador">
-            <img src="../Imagenes/ver.jpg" alt="logo 1">
+            <div class="fig"><img src="../Imagenes/ver.jpg" alt="logo 1"></div>
             <div class="Total_info">
                 2
             </div>
         </div>
         <div class="totalizador">
-            <img src="../Imagenes/ver.jpg" alt="logo 1">
+            <div class="fig"><img src="../Imagenes/ver.jpg" alt="logo 1"></div>
             <div class="Total_info">
                 3
             </div>
         </div>
         <div class="totalizador">
-            <img src="../Imagenes/ver.jpg" alt="logo 1">
+            <div class="fig"><img src="../Imagenes/ver.jpg" alt="logo 1"></div>
             <div class="Total_info">
                 4
             </div>    
