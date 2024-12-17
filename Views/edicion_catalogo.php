aedb5193f0d7b337451fd460cafebc72137ba4fe
Pablo de Dios <4855797@gmail.com>
2024-12-17 17:53:02 -0600

    Pantalla de edición catalogo v72

--- Views/edicion_catalogo.php
+++ Views/edicion_catalogo.php
@@ -215,35 +215,52 @@
 
 <canvas id="speedChart" width="300" height="150"></canvas>
 <script>
-    var speedCanvas = document.getElementById("speedChart");
+    var densityCanvas = document.getElementById("speedChart");
 
     Chart.defaults.global.defaultFontFamily = "Lato";
     Chart.defaults.global.defaultFontSize = 18;
 
-    var speedData = {
-    labels: ["0s", "10s", "20s", "30s", "40s", "50s", "60s"],
-    datasets: [{
-        label: "Car Speed (mph)",
-        data: [0, 59, 75, 20, 20, 55, 40],
-    }]
+    var densityData = {
+    label: 'Density of Planet (kg/m3)',
+    data: [5427, 5243, 5514, 3933, 1326, 687, 1271, 1638],
+    backgroundColor: 'rgba(0, 99, 132, 0.6)',
+    borderColor: 'rgba(0, 99, 132, 1)',
+    yAxisID: "y-axis-density"
     };
-
+    
+    var gravityData = {
+    label: 'Gravity of Planet (m/s2)',
+    data: [3.7, 8.9, 9.8, 3.7, 23.1, 9.0, 8.7, 11.0],
+    backgroundColor: 'rgba(99, 132, 0, 0.6)',
+    borderColor: 'rgba(99, 132, 0, 1)',
+    yAxisID: "y-axis-gravity"
+    };
+    
+    var planetData = {
+    labels: ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"],
+    datasets: [densityData, gravityData]
+    };
+    
     var chartOptions = {
-    legend: {
-        display: true,
-        position: 'top',
-        labels: {
-        boxWidth: 80,
-        fontColor: 'black'
-        }
+    scales: {
+        xAxes: [{
+        barPercentage: 1,
+        categoryPercentage: 0.6
+        }],
+        yAxes: [{
+        id: "y-axis-density"
+        }, {
+        id: "y-axis-gravity"
+        }]
     }
     };
-
-    var lineChart = new Chart(speedCanvas, {
-    type: 'line',
-    data: speedData,
+    
+    var barChart = new Chart(densityCanvas, {
+    type: 'bar',
+    data: planetData,
     options: chartOptions
     });
+
 </script>
 
 <div class="apartado_tabla">
