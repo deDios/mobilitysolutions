326efdde5a8dd0f74031bd7f5ed6fcc451c0e43b
Pablo de Dios <4855797@gmail.com>
2024-12-17 18:29:15 -0600

    Pantalla de edición catalogo v73

--- Views/edicion_catalogo.php
+++ Views/edicion_catalogo.php
@@ -212,6 +212,30 @@
 </div>
 </div>
 
+<?php 
+                $inc = include "../db/Conexion.php";    
+                    if ($inc){
+                        $query = '
+                                select 
+                                    DATE(DATE_SUB(created_at, INTERVAL 6 HOUR)) as fecha, 
+                                    count(*) as numero_cargas
+                                from mobility_solutions.tmx_auto
+                                where DATE(DATE_SUB(created_at, INTERVAL 6 HOUR)) > DATE(DATE_SUB(current_date(), INTERVAL 10 DAY))
+                                group by DATE(DATE_SUB(created_at, INTERVAL 6 HOUR));';
+                        $result = mysqli_query($con,$query);  
+                        if ($result){    
+                            $data_points = array();     
+                            while($row = mysqli_fetch_assoc($result)){
+                                $point = array("valorx" => $row['fecha'], "valory" => $row['numero_cargas']);
+                                array_push($data_points, $point);
+                            }
+                            echo json_encode($data_points);
+                        } else{
+                            echo "Hubo un error en la consulta";
+                        }
+                        mysqli_free_result($result);                  
+                    }
+    ?>
 
 <canvas id="speedChart" width="300" height="150"></canvas>
 <script>
