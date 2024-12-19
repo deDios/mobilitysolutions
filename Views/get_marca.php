6cdbcaf0eb0318c4a9bb622705bdbb6c3a1d3b14
Pablo de Dios <4855797@gmail.com>
2024-12-19 13:54:03 -0600

    Pantalla de edición catalogo v108

---
+++ Views/get_marca.php
@@ -0,0 +1,29 @@
+<?php
+$Marca=$_POST['Marca'];
+    
+    $cadena = "<label for='InputNombre' class='form-label'>Vehiculo</label>
+                <select id='InputNombre' class='form-select' aria-label='Default select example' name='InputNombre'>
+                <option value='0'> Selecciona un Vehiculo </option>
+                ";
+
+$inc = include "db/Conexion.php"; 
+    $query = "select 
+                    id,
+                    marca,
+                    auto
+                FROM mobility_solutions.tmx_marca_auto
+                where marca = '$Marca' order by auto asc;";
+    $result = mysqli_query($con,$query); 
+            if ($result){ 
+                while($row = mysqli_fetch_assoc($result)){
+                        $id = $row['id'];
+                        $auto = $row['auto'];
+                    $id_valor = '"' . $id . '"';
+                    $cadena = $cadena . "<option value=$id_valor> $auto </option>";         
+                }
+            }
+            else{
+                echo "Falla en conexión";
+            }
+            echo $cadena . "</select>";
+?>
---
+++ Views/get_modelo.php
@@ -0,0 +1,31 @@
+<?php
+$Auto=$_POST['Auto'];
+ 
+    $cadena = "<label for='InputModelo' class='form-label'>Modelo</label>
+                <select id='InputModelo' class='form-select' aria-label='Default select example' name='InputModelo'>
+                    <option value='0'> Selecciona un Modelo </option>
+                    ";
+
+$inc = include "db/Conexion.php"; 
+    $query = "select 
+                    id,
+                    automovil,
+                    nombre as modelo
+                FROM mobility_solutions.tmx_modelo
+                where automovil = '$Auto' order by nombre asc;";
+    $result = mysqli_query($con,$query); 
+            if ($result){ 
+                while($row = mysqli_fetch_assoc($result)){
+                        $id = $row['id'];
+                        $modelo = $row['modelo'];
+                    $id_valor = '"' . $id . '"';
+                    //$cadena = $cadena . "<option value=$id_valor> $nombre </option>";
+                    $cadena = $cadena . "<option value=$id_valor> $modelo </option>";         
+                }
+            }
+            else{
+                echo "Falla en conexión";
+            }
+            echo $cadena . "</select>";
+
+?>
