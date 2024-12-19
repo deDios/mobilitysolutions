2f30edfb3f3f943ae197ef73cd5bdc5bfef949b8
Pablo de Dios <4855797@gmail.com>
2024-12-19 09:46:24 -0600

    Pantalla de edición catalogo v96

--- Views/edicion_catalogo.php
+++ Views/edicion_catalogo.php
@@ -218,7 +218,31 @@
 
 </div>
 <div>
-<a href="../insert_reg.php"  class="btn btn-success">+</a>
+<button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModal">
+  +
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 
 <table id="example" class="table table-striped nowrap mt-2" style="width:100%">
     <thead>
