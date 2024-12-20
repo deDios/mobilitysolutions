f7334cbd4d792e9109eac4e91b83c7ecc3e89793
Pablo de Dios <4855797@gmail.com>
2024-12-20 14:26:19 -0600

    Pantalla de edición catalogo v123

--- Views/edicion_catalogo.php
+++ Views/edicion_catalogo.php
@@ -301,6 +301,24 @@
                             <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#exampleModal_auto">
                                 +
                             </button>
+                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Agregar vehiculo a marca</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        ...
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>
                             <select id="InputNombre" class="form-select" aria-label="Default select example" name="InputNombre">
                                 <option value="0">Selecciona un Vehiculo</option> 
                             </select>
