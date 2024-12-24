93d51961314458bd77e4766a307753c3847d03a0
Pablo de Dios <4855797@gmail.com>
2024-12-24 09:56:15 -0600

    Sucursales v26

--- Views/edicion_catalogo.php
+++ Views/edicion_catalogo.php
@@ -141,7 +141,7 @@
                         </ol>
                         </div>
                         <div>
-                        <iframe src="https://www.google.com/maps/d/embed?mid=1tICZQyAbkrtIbcuZ5U8Vf4UiSR8&ehbc=2E312F" width="640" height="480"></iframe>
+                        <iframe src="https://www.google.com/maps/d/embed?mid=1tICZQyAbkrtIbcuZ5U8Vf4UiSR8&ehbc=2E312F" width="600" height="1500"></iframe>
                         </div>
                     </div>                  
                 <!-- Fin de formulario ------------------------------------------------------------->
