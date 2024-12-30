7251e3aeae9ab023b896030b35ccf9f1c7a85094
Pablo de Dios <4855797@gmail.com>
2024-12-30 15:02:49 -0600

    Sucursales v112

--- contact.php
+++ contact.php
@@ -168,7 +168,7 @@
                       <p class="card-subtitle mb-2 text-muted">Activa</p>
                   </div>
               </div>
-              <p class="card-text">Calle Ignacio Allende 512, Santiago Momoxpan, Alvaro Obregón, 72774</p> <br>
+              <p class="card-text">Ignacio Allende 512, Santiago Momoxpan, Alvaro Obregón, 72774</p>
             </div>
           </div>
         </div>
