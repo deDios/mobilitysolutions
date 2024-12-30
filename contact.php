6e47bc4095bbbfef625c8a6a38541a90eb7acf8e
Pablo de Dios <4855797@gmail.com>
2024-12-30 13:44:02 -0600

    Sucursales v93

--- contact.php
+++ contact.php
@@ -96,7 +96,7 @@
                       <p class="card-subtitle mb-2 text-muted">Matriz</p>
                   </div>
               </div>
-              <p class="card-text">Av.</p>
+              <p class="card-text">Av. P. de la Reforma 505 Piso 37, Cuauhtémoc, 06500</p>
             </div>
           </div>
         </div>
@@ -109,12 +109,11 @@
                   </div>
                   <div class="Descripcion">
                       <div class="T_L">
-                        <h6 class="card-title"> CDMX </h6>
+                        <h6 class="card-title"> Sucursal GDL </h6>
                       </div>
-                      <p class="card-subtitle mb-2 text-muted">Matriz</p>
                   </div>
               </div>
-              <p class="card-text">Av.</p>
+              <p class="card-text">Av Rafael Sanzio 150, Camichines Vallarta, 45020</p>
             </div>
           </div>
         </div>
@@ -127,12 +126,11 @@
                   </div>
                   <div class="Descripcion">
                       <div class="T_L">
-                        <h6 class="card-title"> CDMX </h6>
+                        <h6 class="card-title"> Sucursal León </h6>
                       </div>
-                      <p class="card-subtitle mb-2 text-muted">Matriz</p>
                   </div>
               </div>
-              <p class="card-text">Av.</p>
+              <p class="card-text">Blvd. Juan Alonso de Torres Pte. 2002, Valle del Campestre, 37150</p>
             </div>
           </div>
         </div>
@@ -145,12 +143,11 @@
                   </div>
                   <div class="Descripcion">
                       <div class="T_L">
-                        <h6 class="card-title"> CDMX </h6>
+                        <h6 class="card-title"> Sucursal Morelia </h6>
                       </div>
-                      <p class="card-subtitle mb-2 text-muted">Matriz</p>
                   </div>
               </div>
-              <p class="card-text">Av.</p>
+              <p class="card-text">C. Vicente Sta. María 1516, Félix Ireta, 58070</p>
             </div>
           </div>
         </div>
@@ -163,12 +160,11 @@
                   </div>
                   <div class="Descripcion">
                       <div class="T_L">
-                        <h6 class="card-title"> CDMX </h6>
+                        <h6 class="card-title"> Sucursal Puebla </h6>
                       </div>
-                      <p class="card-subtitle mb-2 text-muted">Matriz</p>
                   </div>
               </div>
-              <p class="card-text">Av.</p>
+              <p class="card-text">Calle Ignacio Allende 512, Santiago Momoxpan, Alvaro Obregón, 72774</p>
             </div>
           </div>
         </div>
