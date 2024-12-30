f183c5641663f6ca8e1beccb91999df4c1cd5bc4
Pablo de Dios <4855797@gmail.com>
2024-12-30 13:19:30 -0600

    Sucursales v87

--- contact.php
+++ contact.php
@@ -79,7 +79,7 @@
     <div class="mapa_div">
       <iframe class="mapa shadow-lg mt-n9" src="https://www.google.com/maps/d/embed?mid=1tICZQyAbkrtIbcuZ5U8Vf4UiSR8&ehbc=2E312F" width="100%" height="400"></iframe>
     </div>
-    <div class="ubic p-5 mx-auto">
+    <div class="ubic p-5">
       <div class="ubic_logo">
 
         <div class="cartas pl-3">
