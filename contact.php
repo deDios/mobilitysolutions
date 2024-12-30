ce40534941e29d1462f25d7d198c74778b933c41
Pablo de Dios <4855797@gmail.com>
2024-12-30 12:38:54 -0600

    Sucursales v75

--- contact.php
+++ contact.php
@@ -79,7 +79,7 @@
     <div class="mapa_div">
       <iframe class="mapa shadow-lg mt-n9" src="https://www.google.com/maps/d/embed?mid=1tICZQyAbkrtIbcuZ5U8Vf4UiSR8&ehbc=2E312F" width="100%" height="400"></iframe>
     </div>
-    <div class="ubic col-lg-5 p-5 mx-auto">
+    <div class="ubic col-lg-10 p-5 mx-auto">
       <h1 class="fw-light">Sucursales</h1>
     </div>
   </div>
