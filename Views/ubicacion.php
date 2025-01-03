014ed7cda116fd1714621117eafc28b51e37aab5
Pablo de Dios <4855797@gmail.com>
2025-01-03 17:02:20 -0600

    Sucursales v172

--- Views/ubicacion.php
+++ Views/ubicacion.php
@@ -81,7 +81,7 @@
         class="mapa shadow-lg mt-n9" 
         src="https://www.google.com/maps/d/embed?mid=1tICZQyAbkrtIbcuZ5U8Vf4UiSR8&ehbc=2E312F" 
         width="100%" 
-        height="600"
+        height="700"
         allowfullscreen="false"
         scrolling ="No"
         >
