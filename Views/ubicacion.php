70e9af5842db3a426dd7ba920513bfe980bb71c6
Pablo de Dios <4855797@gmail.com>
2025-01-03 17:12:12 -0600

    Sucursales v174

--- Views/ubicacion.php
+++ Views/ubicacion.php
@@ -87,7 +87,7 @@
         >
     </iframe> 
     <div class="titulo_suc2 mx-auto">
-        <h2 class="fw-light text-center">Encuentra la sucursal de tu preferencias</h2>
+        <h3 class="fw-light text-center">Encuentra la sucursal de tu preferencias</h3>
         </div>
     </div>
     <div class="ubic p-2 mx-auto">
