2257428f21075a66430cf65d158e3d1b982379d3
Pablo de Dios <4855797@gmail.com>
2025-01-03 12:53:17 -0600

    Sucursales v165

--- Views/ubicacion.php
+++ Views/ubicacion.php
@@ -74,7 +74,7 @@
 <div class="sec_map p-2">
     <div class="mapa_div">
         <div class="titulo_suc">
-            <h6>Sucursales</h6>
+            <h4>Sucursales</h4>
         </div>
     <!--    <img class="mapa" src="../Imagenes/ubicaciones/mapa.jpg" width="100%" height="500"> -->
      <iframe 
