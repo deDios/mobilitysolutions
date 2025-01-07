cdafb9f90063ebe7b60ed67cde10a5aa0f3fc084
Pablo de Dios <4855797@gmail.com>
2025-01-07 17:05:15 -0600

    Sucursales v204

--- about_us.php
+++ about_us.php
@@ -438,12 +438,12 @@
 
 <hr class="mt-5 mb-3"/> 
 
-<footer class="mt-5">  
+<footer class="foo mt-5">  
   <div class="container">    
     <div class="row">      
       <div class="col-lg-3">        
         <h6>Conoce más</h6>  
-        <hr class="hr1 mt-2 mb-3"/>      
+        <hr class="hr1 mt-2 mb-3" style="height:5px;border-width:0;color:#FFC00A;background-color:#FFC00A">    
         <ul class="text-secondary list-unstyled">
         <li>
             <a class="text-secondary" href="https://mobilitysolutionscorp.com/about_us.php">¿Quiénes Somos?</a>
@@ -454,11 +454,12 @@
           <li>
             <a class="text-secondary" href="https://mobilitysolutionscorp.com/views/ubicacion.php">Sucursales</a>
           </li>
+          
         </ul>     
       </div>      
       <div class="col-lg-3">        
         <h6>Legales</h6>    
-        <hr class="hr2 mt-2 mb-3"/>    
+        <hr class="hr2 mt-2 mb-3" style="height:5px;border-width:0;color:gainsboro;background-color:gainsboro">     
         <ul class="text-secondary list-unstyled">
           <li>
             <a class="text-secondary" href="/Views/privacy.php">Aviso de privacidad</a>
@@ -467,7 +468,7 @@
       </div>      
       <div class="col-lg-3">        
         <h6>Ayuda</h6>    
-        <hr class="hr3 mt-2 mb-3"/>    
+        <hr class="hr3 mt-2 mb-3" style="height:5px;border-width:0;color:black;background-color:black">    
         <ul class="text-secondary list-unstyled">
           <li>
             <a class="text-secondary" href="https://mobilitysolutionscorp.com/contact.php">Contacto</a>
