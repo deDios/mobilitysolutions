a2a891844bf8075a043130921c81efaa774f78ed
Pablo de Dios <4855797@gmail.com>
2025-01-08 10:33:27 -0600

    Whats en contacto v8

--- contact.php
+++ contact.php
@@ -15,6 +15,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.min.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     
 <!------ Include the above in your HEAD tag ---------->
 </head>
