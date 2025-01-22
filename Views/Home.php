e9b6aad17b98a20f64b2125cf332abf05a49b098
Pablo de Dios <4855797@gmail.com>
2025-01-22 15:30:19 -0600

    login V30

--- Views/Home.php
+++ Views/Home.php
@@ -1,4 +1,16 @@
 <?php
 
+    session_start();
+
+    if (!isset ($_SESSION['username'])){
+        echo ' 
+            <script>
+                alert("Es necesario hacer login, por favor ingrese sus credenciales") ;
+                window.location = "../views/login.php";
+            </script> ';
+            session_destroy();
+            die();
+    }
+
 echo 'Home'
 ?>
