36ede8679d688b5a514758739d158e2592a5ecb1
Pablo de Dios <4855797@gmail.com>
2025-01-22 12:33:04 -0600

    login V4

--- Views/login_usuario_be.php
+++ Views/login_usuario_be.php
@@ -5,9 +5,8 @@
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $query ='Select * from tmx_acceso_usuario 
-    where user_name = '.$username.' 
-    or email = '.$username.'
+    $query ='Select * from mobility_solutions.tmx_acceso_usuario 
+    where user_name = '.$username.'
     and user_password = '.$password.';';
 
     $result = mysqli_query($con,$query); 
