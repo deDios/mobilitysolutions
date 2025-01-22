5fee4e17a4d9db857d2cb81ea05deacd8375bb29
Pablo de Dios <4855797@gmail.com>
2025-01-22 14:08:49 -0600

    login V24

--- contact.php
+++ contact.php
@@ -31,7 +31,7 @@
             <ul class="social-network">
               <li><a class="waves-effect waves-dark" href="https://www.facebook.com/profile.php?id=61563909313215&mibextid=kFxxJD"><i class="fa fa-facebook"></i></a></li>
               <li><a class="waves-effect waves-dark" href="https://mobilitysolutionscorp.com/views/ubicacion.php"><i class="fa fa-map-marker"></i></a></li>
-              <li><a class="waves-effect waves-dark" href="#"><i class="fa fa-user"></i></a></li>
+              <li><a class="waves-effect waves-dark" href="https://mobilitysolutionscorp.com/views/login.php"><i class="fa fa-user"></i></a></li>
             </ul>
           </div>
 
