565b422318a1030f89a51644f74097534819f93f
Pablo de Dios <4855797@gmail.com>
2025-01-22 12:54:56 -0600

    login V8

--- Views/login_usuario_be.php
+++ Views/login_usuario_be.php
@@ -5,20 +5,38 @@
 
     $inc = include "../db/Conexion.php";
 
-    $query ='Select * from mobility_solutions.tmx_acceso_usuario 
-    where user_name = '.$username.'
-    and user_password = '.$password.';';
+    $query ='select 
+                acc.user_id, 
+                acc.user_name, 
+                acc.user_password, 
+                acc.user_type, 
+                acc.r_ejecutivo, 
+                acc.r_editor, 
+                acc.r_autorizador, 
+                acc.r_analista, 
+                us.user_name, 
+                us.second_name, 
+                us.last_name, 
+                us.email, 
+                us.cumpleaños, 
+                us.telefono
+            from mobility_solutions.tmx_acceso_usuario  as acc
+            left join mobility_solutions.tmx_usuario as us
+                on acc.user_id = us.id
+            where acc.user_name = '.$username.'
+                or us.email = '.$username.'
+                and acc.user_password = '.$password.';';
 
     $result = mysqli_query($con,$query); 
+
     if ($result){
-        //header("Location: https://mobilitysolutionscorp.com/views/edicion_catalogo.php", TRUE, 301);
-        echo'Hola';
+        header("Location: https://mobilitysolutionscorp.com/views/edicion_catalogo.php", TRUE, 301);
         exit();
     } else{
         echo ' 
             <script>
                 alert("Usuario no existe, por favor verifique los datos introducidos") ;
-                window.location = "login.php";
+                window.location = "../views/login.php";
             </script> ';
             exit;
         
