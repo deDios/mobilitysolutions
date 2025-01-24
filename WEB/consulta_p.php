79979006b93ffd4d12782aefa250b69032052a08
Pablo de Dios <4855797@gmail.com>
2025-01-24 16:11:40 -0600

    login V44

--- WEB/consulta_p.php
+++ WEB/consulta_p.php
@@ -1,11 +1,11 @@
 <?php
-$cod=$_REQUEST['cod'];
+$cod = $_REQUEST['cod'];
 header('Content-Type: application/json');
 
 $inc = include "../db/Conexion.php";
 
 // Consulta a la base de datos
-$query ='select 
+$query = 'select 
                 id,
                 Categoria, 
                 Nombre, 
@@ -16,18 +16,24 @@ $query ='select
                 atrr_1, 
                 atrr_2, 
                 atrr_3
-            from mobility_solutions.moon_product
-            where Categoria = '. $cod .';';
+            FROM mobility_solutions.moon_product
+            WHERE Categoria = ' . $cod . ';';
 
-$result = mysqli_query($con,$query); 
+$result = mysqli_query($con, $query);
 
 if ($result->num_rows > 0) {
     $data = array();
     
-    while($row = $result->fetch_assoc()) {
+    while ($row = $result->fetch_assoc()) {
+        // Asegúrate de que los valores id, Categoria, y Status sean enteros
+        $row['id'] = (int) $row['id'];
+        $row['Categoria'] = (int) $row['Categoria'];
+        $row['Status'] = (int) $row['Status'];
+        
         $data[] = $row;
     }
 
+    // Enviar los datos como JSON
     echo json_encode($data);
 } else {
     echo json_encode(["mensaje" => "No se encontraron datos"]);
