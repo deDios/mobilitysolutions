93fd4d6829068321f2f01cb6076ee7bbc12a2e6d
Pablo de Dios <4855797@gmail.com>
2025-01-24 13:56:14 -0600

    login V41

--- WEB/consulta_p.php
+++ WEB/consulta_p.php
@@ -1,28 +1,23 @@
 <?php
-
+$cod=$_REQUEST['cod'];
 header('Content-Type: application/json');
 
 $inc = include "../db/Conexion.php";
 
 // Consulta a la base de datos
 $query ='select 
-                acc.user_id, 
-                acc.user_name, 
-                acc.user_password, 
-                acc.user_type, 
-                acc.r_ejecutivo, 
-                acc.r_editor, 
-                acc.r_autorizador, 
-                acc.r_analista, 
-                us.user_name, 
-                us.second_name, 
-                us.last_name, 
-                us.email, 
-                us.cumpleaños, 
-                us.telefono
-            from mobility_solutions.tmx_acceso_usuario  as acc
-            left join mobility_solutions.tmx_usuario as us
-                on acc.user_id = us.id;';
+                id,
+                Categoria, 
+                Nombre, 
+                Precio, 
+                Descripcion, 
+                Imagen_Producto, 
+                Status, 
+                atrr_1, 
+                atrr_2, 
+                atrr_3
+            from mobility_solutions.moon_product
+            where auto.id = '. $cod .';';
 
 $result = mysqli_query($con,$query); 
 
