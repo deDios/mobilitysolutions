fbd6dd5690de7fedbe7c54bb3ebf93dd3e4d778d
Pablo de Dios <4855797@gmail.com>
2025-01-28 09:54:26 -0600

    login V97

--- WEB/cobrar_orden.php
+++ WEB/cobrar_orden.php
@@ -2,16 +2,6 @@
 include('../db/Conexion_p.php'); 
 $ssl_ca = '../db/db/DigiCertGlobalRootCA.crt.pem';  // El certificado de la autoridad certificadora (CA)
 
-$conn = mysqli_init();
-mysqli_ssl_set($conn, NULL, NULL, $ssl_ca, NULL, NULL);  // Solo proporcionamos el CA
-
-mysqli_real_connect($conn, $hostname, $username, $password, $database);
-
-if (mysqli_connect_errno()) {
-    echo json_encode(['status' => 'error', 'message' => 'Error de conexión: ' . mysqli_connect_error()]);
-    exit;
-}
-
 $data = json_decode(file_get_contents('php://input'), true);  // Asumiendo que los datos se envían como JSON
 
 if (isset($data['id_cliente'], $data['nombre_cliente'], $data['productos']) && is_array($data['productos'])) {
@@ -21,18 +11,18 @@ if (isset($data['id_cliente'], $data['nombre_cliente'], $data['productos']) && i
     
     $productos = $data['productos'];
 
-    mysqli_begin_transaction($conn);
+    mysqli_begin_transaction($con);
 
     $query_insert = "insert into moon_ventas (id_cliente, nombre_cliente) VALUES (?, ?)";
-    if ($stmt = mysqli_prepare($conn, $query_insert)) {
+    if ($stmt = mysqli_prepare($con, $query_insert)) {
         mysqli_stmt_bind_param($stmt, "is", $id_cliente, $nombre_cliente);
         $result = mysqli_stmt_execute($stmt);
         if (!$result) {
             echo json_encode(['status' => 'error', 'message' => 'Error al insertar la orden']);
-            mysqli_rollback($conn);
+            mysqli_rollback($con);
             exit;
         }
-        $orden_id = mysqli_insert_id($conn);  
+        $orden_id = mysqli_insert_id($con);  
         mysqli_stmt_close($stmt);
     }
 
@@ -40,7 +30,7 @@ if (isset($data['id_cliente'], $data['nombre_cliente'], $data['productos']) && i
     $query_producto = "insert into moon_ventas (folio, id_cliente, nombre_cliente, id_producto, producto, cantidad, precio_unitario, sub_total) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
     foreach ($productos as $producto) {
         if (isset($producto['folio'], $producto['producto'], $producto['id_producto'], $producto['cantidad'], $producto['precio_unitario'], $producto['total'])) {
-            if ($stmt = mysqli_prepare($conn, $query_producto)) {
+            if ($stmt = mysqli_prepare($con, $query_producto)) {
                 mysqli_stmt_bind_param(
                     $stmt,
                     "sisdiidd",
@@ -56,26 +46,24 @@ if (isset($data['id_cliente'], $data['nombre_cliente'], $data['productos']) && i
                 $result = mysqli_stmt_execute($stmt);
                 if (!$result) {
                     echo json_encode(['status' => 'error', 'message' => 'Error al insertar el producto']);
-                    mysqli_rollback($conn);
+                    mysqli_rollback($con);
                     exit;
                 }
                 mysqli_stmt_close($stmt);
             }
         } else {
             echo json_encode(['status' => 'error', 'message' => 'Producto con datos incompletos']);
-            mysqli_rollback($conn);
+            mysqli_rollback($con);
             exit;
         }
     }
-
-    // Confirmar la transacción
-    if (!mysqli_commit($conn)) {
+    if (!mysqli_commit($con)) {
         echo json_encode(['status' => 'error', 'message' => 'Error al confirmar la transacción']);
         exit;
     }
 
     // Cerrar la conexión
-    mysqli_close($conn);
+    mysqli_close($con);
 
     // Responder con éxito
     echo json_encode(['status' => 'success', 'message' => 'Orden registrada correctamente']);
