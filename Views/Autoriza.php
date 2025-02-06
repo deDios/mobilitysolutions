3ff3943039a61dfa8e0ed7ec6667243150ed851f
Pablo de Dios <4855797@gmail.com>
2025-02-06 02:18:59 -0600

    Color en detalle  v9

--- Views/Autoriza.php
+++ Views/Autoriza.php
@@ -75,7 +75,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit</title>
+    <title>Aprobaciones</title>
     <link rel="shortcut icon" href="../Imagenes/movility.ico" />
     <link rel="stylesheet" href="../CSS/autoriza.css">
 
