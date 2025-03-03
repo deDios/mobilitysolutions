eac20f06058457a704d020f7472f18e2ac555534
Pablo de Dios <4855797@gmail.com>
2025-03-03 15:06:27 -0600

    Pag de requerimientos v13

--- Views/requerimiento.php
+++ Views/requerimiento.php
@@ -242,7 +242,7 @@ if (isset($_POST['verificar'])) {
                 </div>
                 <?php endif; ?>
 
-                <button class="btn py-2 btn-success" type="submit" <?php echo $disabled; ?>>Enviar</button>
+                <button class="btn py-5 btn-success" type="submit" <?php echo $disabled; ?>>Solicitar reserva</button>
 
             </form>
         <?php elseif ($selected == '2'): ?>
