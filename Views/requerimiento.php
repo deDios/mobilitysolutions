37f7130cd3daaebd418cddae68a161001d74d3d8
Pablo de Dios <4855797@gmail.com>
2025-03-03 14:06:22 -0600

    Pag de requerimientos v3

--- Views/requerimiento.php
+++ Views/requerimiento.php
@@ -66,7 +66,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aprobaciones</title>
+    <title>Requerimientos</title>
     <link rel="shortcut icon" href="../Imagenes/movility.ico" />
     <link rel="stylesheet" href="../CSS/Req.css">
 
