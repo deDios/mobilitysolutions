cc5992a0dc8c749f62579613fb0fcb7469bcd2d5
Pablo de Dios <4855797@gmail.com>
2025-03-03 17:09:24 -0600

    Pag de requerimientos v30

--- Views/requerimiento.php
+++ Views/requerimiento.php
@@ -1,65 +1,3 @@
-<?php
-
-    session_start();
-
-    if (!isset ($_SESSION['username'])){
-        echo ' 
-            <script>
-                alert("Es necesario hacer login, por favor ingrese sus credenciales") ;
-                window.location = "../views/login.php";
-            </script> ';
-            session_destroy();
-            die();
-    }
-
-    $inc = include "../db/Conexion.php";
-
-    $query ='select 
-                acc.user_id, 
-                acc.user_name, 
-                acc.user_password, 
-                acc.user_type, 
-                acc.r_ejecutivo, 
-                acc.r_editor, 
-                acc.r_autorizador, 
-                acc.r_analista, 
-                us.user_name as nombre, 
-                us.second_name as s_nombre, 
-                us.last_name, 
-                us.email, 
-                us.cumpleaños, 
-                us.telefono
-            from mobility_solutions.tmx_acceso_usuario  as acc
-            left join mobility_solutions.tmx_usuario as us
-                on acc.user_id = us.id
-            where acc.user_name = '.$_SESSION['username'].';';
-
-    $result = mysqli_query($con,$query); 
-
-    if ($result){ 
-        while($row = mysqli_fetch_assoc($result)){
-                            $user_id = $row['user_id'];
-                            $user_name = $row['user_name'];
-                            $user_password = $row['user_password'];
-                            $user_type = $row['user_type'];
-                            $r_ejecutivo = $row['r_ejecutivo'];
-                            $r_editor = $row['r_editor'];
-                            $r_autorizador = $row['r_autorizador'];
-                            $r_analista = $row['r_analista'];
-                            $nombre = $row['nombre'];
-                            $s_nombre = $row['s_nombre'];
-                            $last_name = $row['last_name'];
-                            $email = $row['email'];
-                            $cumpleaños = $row['cumpleaños'];
-                            $telefono = $row['telefono'];
-                           
-        }
-    }
-    else{
-        echo 'Falla en conexión.';
-    }
-
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -153,6 +91,63 @@
 </div>
 
 <?php
+    session_start();
+
+    if (!isset ($_SESSION['username'])){
+        echo ' 
+            <script>
+                alert("Es necesario hacer login, por favor ingrese sus credenciales") ;
+                window.location = "../views/login.php";
+            </script> ';
+            session_destroy();
+            die();
+    }
+    $inc = include "../db/Conexion.php";
+    $query ='select 
+                acc.user_id, 
+                acc.user_name, 
+                acc.user_password, 
+                acc.user_type, 
+                acc.r_ejecutivo, 
+                acc.r_editor, 
+                acc.r_autorizador, 
+                acc.r_analista, 
+                us.user_name as nombre, 
+                us.second_name as s_nombre, 
+                us.last_name, 
+                us.email, 
+                us.cumpleaños, 
+                us.telefono
+            from mobility_solutions.tmx_acceso_usuario  as acc
+            left join mobility_solutions.tmx_usuario as us
+                on acc.user_id = us.id
+            where acc.user_name = '.$_SESSION['username'].';';
+
+    $result = mysqli_query($con,$query); 
+
+    if ($result){ 
+        while($row = mysqli_fetch_assoc($result)){
+                            $user_id = $row['user_id'];
+                            $user_name = $row['user_name'];
+                            $user_password = $row['user_password'];
+                            $user_type = $row['user_type'];
+                            $r_ejecutivo = $row['r_ejecutivo'];
+                            $r_editor = $row['r_editor'];
+                            $r_autorizador = $row['r_autorizador'];
+                            $r_analista = $row['r_analista'];
+                            $nombre = $row['nombre'];
+                            $s_nombre = $row['s_nombre'];
+                            $last_name = $row['last_name'];
+                            $email = $row['email'];
+                            $cumpleaños = $row['cumpleaños'];
+                            $telefono = $row['telefono'];
+                           
+        }
+    }
+    else{
+        echo 'Falla en conexión.';
+    }
+
 header('Content-Type: application/json');  // Asegura que la respuesta será JSON
 
 $selected = isset($_GET['req']) ? $_GET['req'] : '1';
