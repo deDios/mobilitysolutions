8526155a0285f58f38327936c0eb63201fac4917
Pablo de Dios <4855797@gmail.com>
2025-03-04 12:23:12 -0600

    Pag de requerimientos v54

--- Views/requerimientos.php
+++ Views/requerimientos.php
@@ -288,10 +288,9 @@ if (isset($_POST['verificar'])) {
             }
 
             const vehiculo = JSON.parse(vehiculoData);
-            const usuario = { id: parseInt(usuarioData) };  // Aquí agregamos la propiedad id
+            const usuario = { id: usuarioData };  // Aquí agregamos la propiedad id
 
             console.log("Usuario ID:", usuario.id);
-
             fetch("../db_consultas/insert_sp_req.php", {
                 method: "POST",
                 headers: {
