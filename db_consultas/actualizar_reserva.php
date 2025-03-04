29fcd1c62ae260e4b52b8a528b6b1bbedcc42322
Pablo de Dios <4855797@gmail.com>
2025-03-04 14:49:58 -0600

    Pag de requerimientos v68

--- db_consultas/actualizar_reserva.php
+++ db_consultas/actualizar_reserva.php
@@ -3,29 +3,50 @@ header('Content-Type: application/json'); // Asegurar que la respuesta sea JSON
 include "../db/Conexion.php";
 
 session_start();
-if (!isset($_SESSION['username'])) {
-    echo json_encode(["success" => false, "message" => "Es necesario hacer login, por favor ingrese sus credenciales"]);
-    exit;
-}
+$query ='select 
+                acc.user_id, 
+                acc.user_name, 
+                acc.user_password, 
+                acc.user_type, 
+                acc.r_ejecutivo, 
+                acc.r_editor, 
+                acc.r_autorizador, 
+                acc.r_analista, 
+                us.user_name as nombre, 
+                us.second_name as s_nombre, 
+                us.last_name, 
+                us.email, 
+                us.cumpleaños, 
+                us.telefono
+            from mobility_solutions.tmx_acceso_usuario  as acc
+            left join mobility_solutions.tmx_usuario as us
+                on acc.user_id = us.id
+            where acc.user_name = '.$_SESSION['username'].';';
 
-if (!$con) {
-    echo json_encode(["success" => false, "message" => "Falla en la conexión a la base de datos"]);
-    exit;
-}
+    $result = mysqli_query($con,$query); 
 
-$query_user = 'SELECT user_id FROM mobility_solutions.tmx_acceso_usuario WHERE user_name = ?';
-$stmt_user = mysqli_prepare($con, $query_user);
-
-mysqli_stmt_bind_param($stmt_user, "s", $_SESSION['username']);
-mysqli_stmt_execute($stmt_user);
-$result_user = mysqli_stmt_get_result($stmt_user);
-
-if ($result_user && $row = mysqli_fetch_assoc($result_user)) {
-    $user_id = $row['user_id'];
-} else {
-    echo json_encode(["success" => false, "message" => "No se encontró el ID del usuario"]);
-    exit;
-}
+    if ($result){ 
+        while($row = mysqli_fetch_assoc($result)){
+                            $user_id = $row['user_id'];
+                            $user_name = $row['user_name'];
+                            $user_password = $row['user_password'];
+                            $user_type = $row['user_type'];
+                            $r_ejecutivo = $row['r_ejecutivo'];
+                            $r_editor = $row['r_editor'];
+                            $r_autorizador = $row['r_autorizador'];
+                            $r_analista = $row['r_analista'];
+                            $nombre = $row['nombre'];
+                            $s_nombre = $row['s_nombre'];
+                            $last_name = $row['last_name'];
+                            $email = $row['email'];
+                            $cumpleaños = $row['cumpleaños'];
+                            $telefono = $row['telefono'];
+                           
+        }
+    }
+    else{
+        echo 'Falla en conexión.';
+    }
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $id = isset($_POST['id']) ? $_POST['id'] : null;
