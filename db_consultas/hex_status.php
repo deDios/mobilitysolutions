2df0d3605ba85d8c337873f54e84a549aa4ec979
Pablo de Dios <4855797@gmail.com>
2025-04-24 15:25:36 -0600

    hexagonos v8

--- db_consultas/hex_status.php
+++ db_consultas/hex_status.php
@@ -1,5 +1,6 @@
 <?php
-$user_id = $_GET['user_id']; // Cambié 'cod' por 'user_id'
+$user_id = $_GET['user_id']; 
+
 header('Content-Type: application/json');
 
 // Asegúrate de que el valor user_id sea un número entero
@@ -11,12 +12,20 @@ if (!is_numeric($user_id)) {
 $inc = include "../db/Conexion.php";
 
 // Consulta a la base de datos
-$query = 'SELECT 
-                COUNT(CASE WHEN tipo = "Nuevo" THEN 1 END) as nuevo,
-                COUNT(CASE WHEN tipo = "Reserva" THEN 1 END) as reserva,
-                COUNT(CASE WHEN tipo = "Entrega" THEN 1 END) as entrega
-            FROM tmx_servicio
-            WHERE estatus = 2 AND (created_by = ' . $user_id . ' OR some_other_column = ' . $user_id . ')'; // Aquí ajusté la consulta
+$query = "SELECT
+            MONTHNAME(req_created_at) AS Mes,
+            SUM(CASE WHEN tipo_req = 'Nuevo en catálogo' THEN 1 ELSE 0 END) AS New,
+            SUM(CASE WHEN tipo_req = 'Reserva de vehículo' THEN 1 ELSE 0 END) AS Reserva,
+            SUM(CASE WHEN tipo_req = 'Entrega de vehículo' THEN 1 ELSE 0 END) AS Entrega
+        FROM 
+        mobility_solutions.tmx_requerimiento
+        WHERE 
+        YEAR(req_created_at) = YEAR(CURDATE()) AND estatus = 2 AND created_by = $user_id
+        GROUP BY 
+        MONTH(req_created_at),
+        MONTHNAME(req_created_at)
+        ORDER BY 
+        MONTH(req_created_at)"; 
 
 $result = mysqli_query($con, $query);
 
