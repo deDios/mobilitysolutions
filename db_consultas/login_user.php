27fba32b35ef5763961c6bef988157272031e0dd
Pablo de Dios <4855797@gmail.com>
2025-07-21 19:26:54 -0600

    Cambio en login v3

--- db_consultas/login_user.php
+++ db_consultas/login_user.php
@@ -1,48 +1,52 @@
 <?php
-session_start();
+    session_start();
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-$inc = include "../db/Conexion.php";
+    $username_valor = '"' . $username . '"';
+    $password_valor = '"' . $password . '"';
 
-// Escapamos para evitar SQL Injection
-$username_esc = mysqli_real_escape_string($con, $username);
-$password_esc = mysqli_real_escape_string($con, $password);
 
-$query = "SELECT 
-            acc.user_id, 
-            acc.user_name, 
-            acc.user_password, 
-            acc.user_type, 
-            acc.r_ejecutivo, 
-            acc.r_editor, 
-            acc.r_autorizador, 
-            acc.r_analista, 
-            us.user_name AS nombre_real, 
-            us.second_name, 
-            us.last_name, 
-            us.email, 
-            us.cumpleaños, 
-            us.telefono
-        FROM mobility_solutions.tmx_acceso_usuario acc
-        LEFT JOIN mobility_solutions.tmx_usuario us
-            ON acc.user_id = us.id
-        WHERE 
-            (acc.user_name = '$username_esc' OR us.email = '$username_esc')
-            AND acc.user_password = '$password_esc'";
+    $inc = include "../db/Conexion.php";
 
-$result = mysqli_query($con, $query);
+    $query ='select 
+                acc.user_id, 
+                acc.user_name, 
+                acc.user_password, 
+                acc.user_type, 
+                acc.r_ejecutivo, 
+                acc.r_editor, 
+                acc.r_autorizador, 
+                acc.r_analista, 
+                us.user_name, 
+                us.second_name, 
+                us.last_name, 
+                us.email, 
+                us.cumpleaños, 
+                us.telefono
+            from mobility_solutions.tmx_acceso_usuario  as acc
+            left join mobility_solutions.tmx_usuario as us
+                on acc.user_id = us.id
+            where acc.user_name = '.$username_valor.'
+                or us.email = '.$username_valor.'
+                and acc.user_password = '.$password_valor.';';
 
-if (mysqli_num_rows($result) > 0) {
-    $_SESSION['username'] = $username;
-    header("Location: https://mobilitysolutionscorp.com/views/Home.php", TRUE, 301);
-    exit();
-} else {
-    echo '<script>
-        alert("Usuario o contraseña incorrectos. Verifique sus datos.");
-        window.location = "../views/login.php";
-    </script>';
-    exit();
-}
+    $result = mysqli_query($con,$query); 
+
+    if (mysqli_num_rows($result) > 0 ){
+        $_SESSION['username'] = $username_valor;
+        header("Location: https://mobilitysolutionscorp.com/views/Home.php", TRUE, 301);
+        exit();
+    } else{
+        echo ' 
+            <script>
+                alert("Usuario no existe, por favor verifique sus datos") ;
+                window.location = "../views/login.php";
+            </script> ';
+            exit();
+        
+    }
+
+
 ?>
