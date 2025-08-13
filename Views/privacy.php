796ec1a164ad8bca0fd506d71265a3081bc1eb4d
Pablo de Dios <4855797@gmail.com>
2025-08-13 10:21:50 -0600

    Tag de google v1

--- Views/privacy.php
+++ Views/privacy.php
@@ -40,6 +40,16 @@ $pdf_file = "/DOCS/AP.pdf";
     <!-- End Meta Pixel Code -->
     <!------ Include the above in your HEAD tag ---------->
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-C7J5YGXNDS">
+    </script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-C7J5YGXNDS');
+    </script>
 </head>
 <body>
    
--- Views/vende.php
+++ Views/vende.php
@@ -38,6 +38,17 @@
     /></noscript>
     <!-- End Meta Pixel Code -->
     <!------ Include the above in your HEAD tag ---------->
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-C7J5YGXNDS">
+    </script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-C7J5YGXNDS');
+    </script>
 </head>
 <body>
 
