bd000eb23d7f80bb7ea2b007562ff1fd7c5b6f33
Pablo de Dios <4855797@gmail.com>
2025-09-14 10:32:01 -0600

    revision API v11

--- db/Conexion.php
+++ db/Conexion.php
@@ -1,11 +1,17 @@
 <?php
+$host = "mobilitysolutions-server.mysql.database.azure.com";
+$user = "btdonyajwn";                     // sin @server
+$pass = "Llaverito_4855797'?";             // actualízalo
+$db   = "mobility_solutions";
+$port = 3306;
+$caPath = "/home/site/wwwroot/db/DigiCertGlobalRootG2.crt.pem";
 
 $con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "/site/wwwroot/db/DigiCertGlobalRootC2.crt.pem", NULL, NULL);
-mysqli_real_connect($con, "mobilitysolutions-server.mysql.database.azure.com", "btdonyajwn", "Llaverito_4855797'?", "mobility_solutions", 3306, MYSQLI_CLIENT_SSL);
+mysqli_options($con, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
+mysqli_ssl_set($con, NULL, NULL, $caPath, NULL, NULL);
+mysqli_real_connect($con, $host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL);
+
 if (mysqli_connect_errno()){
-    die('Error: Falla en la conexion de MySQL'. mysqli_connect_errno());
+    die('Error: Falla en la conexión de MySQL: ' . mysqli_connect_error());
 }
-
-
 ?>
