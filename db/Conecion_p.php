9263c48540542dddc89371e6c9a7f00c773c58fa
Pablo de Dios <4855797@gmail.com>
2025-09-14 09:51:20 -0600

    revision API v3

--- db/Conecion_p.php
+++ db/Conecion_p.php
@@ -1,7 +1,7 @@
 <?php
 
 $con = mysqli_init();
-mysqli_ssl_set($con, NULL, NULL, "/home/site/wwwroot/db/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_ssl_set($con, NULL, NULL, "/home/site/wwwroot/db/DigiCertGlobalRootC2.crt.pem", NULL, NULL);
 mysqli_real_connect($con, "mobilitysolutions-server.mysql.database.azure.com", "btdonyajwn", "Llaverito_4855797'?", "mobility_solutions", 3306, MYSQLI_CLIENT_SSL);
 if (mysqli_connect_errno()) {
     die('Error: Falla en la conexión de MySQL. ' . mysqli_connect_error());
