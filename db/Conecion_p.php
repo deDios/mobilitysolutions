af116499ba9b99d870d9102ff5db737db46e6ad7
Pablo Agustin de Dios Garcia <4855797@gmail.com>
2025-09-30 13:50:43 -0600

    Update Conecion_p.php

--- db/Conecion_p.php
+++ db/Conecion_p.php
@@ -2,7 +2,7 @@
 
 $con = mysqli_init();
 mysqli_ssl_set($con, NULL, NULL, "/home/site/wwwroot/db/DigiCertGlobalRootC2.crt.pem", NULL, NULL);
-mysqli_real_connect($con, "mobilitysolutions-server.mysql.database.azure.com", "btdonyajwn", "Llaverito_4855797'?", "mobility_solutions", 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($con, "mobilitysolutions-server.mysql.database.azure.com", "btdonyajwn", "Llaverito_4855797", "mobility_solutions", 3306, MYSQLI_CLIENT_SSL);
 if (mysqli_connect_errno()) {
     die('Error: Falla en la conexión de MySQL. ' . mysqli_connect_error());
 }
