d6eca17f8f7e5a995bab24090a5651d5213cd2a5
Pablo de Dios <4855797@gmail.com>
2025-12-22 12:28:06 -0600

    remake a home v7

--- Views/Home_v2.php
+++ Views/Home_v2.php
@@ -70,7 +70,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- CSS específico de Home -->
-    <link rel="stylesheet" href="../CSS/home.css">
+    <link rel="stylesheet" href="../CSS/home_v2.css">
 
     <!-- Bootstrap / jQuery / Chart.js -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
